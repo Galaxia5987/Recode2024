743109f067c80cc5f75c7274a338b148e50f58c4
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-06-22 13:04:36 +0300

    Fix unit testing structure

--- src/test/java/frc/robot/subsystems/example/ExampleSubsystemTest.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems.example;
-
-import org.junit.Test;
-
-public class ExampleSubsystemTest {
-
-    @Test
-    public void setPower() {}
-}
---
+++ src/test/kotlin/frc/robot/subsystems/example/ExampleSubsystemTest.kt
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.example
+
+import org.junit.jupiter.api.Test
+
+class ExampleSubsystemTest {
+    @Test
+    fun setPower() {
+    }
+}
