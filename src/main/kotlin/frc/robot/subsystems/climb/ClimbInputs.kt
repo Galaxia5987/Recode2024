4d4a0bacef3ba8787d81332aab87be24e575f768
Emma Livne <emmalivne.03@gmail.com>
2024-06-28 14:05:26 +0300

    Add And Log climb inputs

--- src/main/kotlin/frc/robot/subsystems/climb/ClimbInputs.kt
+++ src/main/kotlin/frc/robot/subsystems/climb/ClimbInputs.kt
@@ -1,11 +1,14 @@
 package frc.robot.subsystems.climb
 
-import edu.wpi.first.units.Angle
+import edu.wpi.first.units.Current
 import edu.wpi.first.units.MutableMeasure
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.Voltage
+import org.team9432.annotation.Logged
 
-class ClimbInputs {
-    var stopperSetpoint : MutableMeasure<Angle> = Units.Degree.of(0.0).mutableCopy()
-    var appliedVoltage : MutableMeasure<Voltage> = Units.Volts.of(0.0).mutableCopy()
+@Logged
+open class ClimbInputs {
+    var stopperAppliedVoltage: MutableMeasure<Voltage> = Units.Volts.of(0.0).mutableCopy()
+    var stopperCurrent: MutableMeasure<Current> = Units.Amps.of(0.0).mutableCopy()
+    var mainMotorAppliedVoltage: MutableMeasure<Voltage> = Units.Volts.of(0.0).mutableCopy()
 }
