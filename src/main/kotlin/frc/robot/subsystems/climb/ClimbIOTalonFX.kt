076dc1fb8c526f3163ec33b13f53aeaff5be6925
Emma Livne <emmalivne.03@gmail.com>
2024-06-28 14:04:34 +0300

    Add functions to real IO

--- src/main/kotlin/frc/robot/subsystems/climb/ClimbIOTalonFX.kt
+++ src/main/kotlin/frc/robot/subsystems/climb/ClimbIOTalonFX.kt
@@ -1,36 +1,56 @@
 package frc.robot.subsystems.climb
 
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode
+import com.ctre.phoenix6.controls.StrictFollower
 import com.ctre.phoenix6.hardware.TalonFX
-import edu.wpi.first.wpilibj.Servo
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
+import com.ctre.phoenix6.controls.DutyCycleOut
+import edu.wpi.first.units.MutableMeasure
+import edu.wpi.first.units.Units
 import frc.robot.Ports
-import frc.robot.main
 
 class ClimbIOTalonFX : ClimbIO {
-    private val mainMotor = TalonFX(Ports.Climb.mainMotorID)
-    private val auxMotor = TalonFX(Ports.Climb.auxMotorID)
-    private val servo = Servo(Ports.Climb.servoID)
+    private val mainMotor = TalonFX(Ports.Climb.MAIN_MOTOR_ID)
+    private val auxMotor = TalonFX(Ports.Climb.AUX_MOTOR_ID)
+    private val stopperMotor = TalonSRX(Ports.Climb.STOPPER_ID)
+
+    private val percentOutput = DutyCycleOut(0.0).withEnableFOC(true)
+
+    override var isStopperStuck = false
+        get() = stopperMotor.statorCurrent>=ClimbConstants.STOPPER_MOTOR_CURRENT_THRESHOLD
 
     init {
-        mainMotor.configurator.apply()
+        mainMotor.configurator.apply(ClimbConstants.MOTOR_CONFIG)
+        auxMotor.configurator.apply(ClimbConstants.MOTOR_CONFIG)
+        auxMotor.setControl(StrictFollower(mainMotor.deviceID))
+
+        stopperMotor.configFactoryDefault()
+        stopperMotor.enableCurrentLimit(true)
+        stopperMotor.enableVoltageCompensation(true)
+        stopperMotor.configVoltageCompSaturation(12.0)
+        stopperMotor.configPeakCurrentLimit(ClimbConstants.STOPPER_MOTOR_CURRENT_LIMIT)
+        stopperMotor.inverted = true
     }
 
     override fun setPower(power: Double) {
-        TODO("Not yet implemented")
+        mainMotor.setControl(percentOutput.withOutput(power))
     }
 
     override fun openStopper() {
-        TODO("Not yet implemented")
+        stopperMotor.set(TalonSRXControlMode.PercentOutput, 0.5)
     }
 
     override fun closeStopper() {
-        TODO("Not yet implemented")
+        stopperMotor.set(TalonSRXControlMode.PercentOutput, -0.5)
     }
 
     override fun disableStopper() {
-        TODO("Not yet implemented")
+        stopperMotor.set(TalonSRXControlMode.Disabled, 0.0)
     }
 
-    override fun updateInputs() {
-        TODO("Not yet implemented")
+    override fun updateInputs(inputs: ClimbInputs) {
+        inputs.stopperAppliedVoltage.mut_replace(stopperMotor.motorOutputVoltage, Units.Volt)
+        inputs.stopperCurrent.mut_replace(stopperMotor.statorCurrent, Units.Amps)
+        inputs.mainMotorAppliedVoltage.mut_replace(mainMotor.motorVoltage.value, Units.Volt)
     }
 }
