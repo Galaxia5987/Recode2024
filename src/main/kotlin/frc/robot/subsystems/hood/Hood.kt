6c10c26394658cefd1f1ec29e2edb013e06eb6c2
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-07-05 13:30:00 +0300

    Implemented Hood Methods

--- src/main/kotlin/frc/robot/subsystems/hood/Hood.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/Hood.kt
@@ -1,10 +1,14 @@
 package frc.robot.subsystems.hood
 
+import edu.wpi.first.units.Angle
+import edu.wpi.first.units.MutableMeasure
 import edu.wpi.first.wpilibj.Timer
+import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.littletonrobotics.junction.Logger
 
-class Hood private constructor(io: HoodIO): SubsystemBase() {
-    private val inputs: LoggedHoodInputs = HoodIO.inputs
+class Hood private constructor(private val io: HoodIO): SubsystemBase() {
+    private val inputs: LoggedHoodInputs = io.inputs
     private val timer = Timer()
     private val encoderTimer = Timer()
 
@@ -36,4 +40,23 @@ class Hood private constructor(io: HoodIO): SubsystemBase() {
     }
 
     fun atSetpoint() : Boolean = inputs.absoluteEncoderAngle.isNear(inputs.angleSetpoint, HoodConstants.MAX_TOLERANCE_DEG)
+
+    fun getAngle(): MutableMeasure<Angle> = inputs.internalAngle
+
+    fun setAngle(angle: MutableMeasure<Angle>): Command {
+        return run {
+            inputs.angleSetpoint.mut_replace(angle)
+            io.setAngle(angle)
+        }.withName("Set Angle Hood")
+    }
+
+    fun setRestingAngle(): Command = setAngle(HoodConstants.RESTING_ANGLE).withName("Set Resting Angle Hood")
+
+    override fun periodic() {
+        io.updateInputs()
+        if (encoderTimer.advanceIfElapsed(0.5)) io.updateInternalEncoder()
+        Logger.processInputs("Hood", inputs)
+
+
+    }
 }
