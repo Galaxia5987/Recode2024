3c4c2e04d4df0a04ffd5c752d8979cb50bc60837
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-07-05 13:30:12 +0300

    Implemented HoodIOReal Methods

--- src/main/kotlin/frc/robot/subsystems/hood/HoodIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodIOReal.kt
@@ -1,35 +1,57 @@
 package frc.robot.subsystems.hood
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC
 import com.ctre.phoenix6.hardware.TalonFX
+import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.units.Angle
 import edu.wpi.first.units.MutableMeasure
+import edu.wpi.first.units.Units
+import frc.robot.lib.Utils
+import kotlin.math.sign
 
 class HoodIOReal : HoodIO {
     private val motor: TalonFX = TalonFX(-1) // TODO: Replace with real motor port
-    private val encoder = (-1) // TODO: Replace with real Port
+    private val encoder = TalonSRX(-1) // TODO: Replace with real Port
     private val positionControl = PositionTorqueCurrentFOC(0.0)
 
     init {
         motor.configurator.apply(HoodConstants.MOTOR_CONFIGURATION)
 
+        encoder.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute)
     }
 
-    private fun getEncoderPosition() : Double = 0.0 // TODO: Implement
+    private fun getEncoderPosition(): Double {
+        val encoderTicksPerRevolution = HoodConstants.ENCODER_TICKS_PER_REVOLUTION.toDouble()
+        val encoderPosition = encoder.selectedSensorPosition % encoderTicksPerRevolution
+        val normalizedPosition = encoderPosition / encoderTicksPerRevolution - HoodConstants.ABSOLUTE_ENCODER_OFFSET.get()
+        return Utils.normalize(Rotation2d.fromRotations(normalizedPosition)).rotations
+    }
 
     override fun updateInternalEncoder() {
-        // TODO: Implement
+        motor.setPosition(getEncoderPosition())
     }
 
     override fun setAngle(angle: MutableMeasure<Angle>) {
-        // TODO: Implement
-    }
-
-    override fun setAngle(angle: MutableMeasure<Angle>, torqueChassisCompensation: Double) {
-        // TODO: Implement
+        val error = angle.minus(inputs.absoluteEncoderAngle)
+        motor.setControl(
+            positionControl
+                .withPosition(inputs.internalAngle.plus(error).`in`(Units.Rotations))
+                .withFeedForward(
+                    sign(
+                        inputs.angleSetpoint.minus(inputs.absoluteEncoderAngle
+                        ).`in`(Units.Degrees)) * HoodConstants.kS.get())
+        )
     }
 
     override fun updateInputs() {
-        // TODO: Implement
+        inputs.internalAngle.mut_replace(motor.position.value, Units.Rotations)
+        inputs.absoluteEncoderAngle.mut_replace(getEncoderPosition(), Units.Rotations)
+        inputs.voltage.mut_replace(motor.motorVoltage.value, Units.Volts)
+        inputs.absoluteEncoderAngleNoOffset.mut_replace(
+            ((encoder.getSelectedSensorPosition() % HoodConstants.ENCODER_TICKS_PER_REVOLUTION)
+                    / HoodConstants.ENCODER_TICKS_PER_REVOLUTION),
+            Units.Rotations)
     }
 }
