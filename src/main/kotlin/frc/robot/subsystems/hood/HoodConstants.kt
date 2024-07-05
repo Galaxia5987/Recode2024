c9aab099f90d53dc51bc1b15485d3704628f2b29
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-07-05 13:28:41 +0300

    Add ENCODER_TICKS_PER_REVOLUTION constant

--- src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
@@ -7,6 +7,7 @@ import frc.robot.lib.webconstants.LoggedTunableNumber
 object HoodConstants {
     const val GEAR_RATIO : Double = 3.0 * (36.0 / 18.0) * (158.0 / 18.0)
     const val MAX_TOLERANCE_DEG = 0.75 // Degrees
+    const val ENCODER_TICKS_PER_REVOLUTION = 4096
 
     val MOMENT_OF_INERTIA : Measure<Mult<Mult<Mass, Distance>, Distance>> = Units.Kilograms.mult(Units.Meters).mult(Units.Meters).of(0.0003);
     val MAX_VELOCITY: Measure<Velocity<Angle>> = Units.RotationsPerSecond.of(1.0)
