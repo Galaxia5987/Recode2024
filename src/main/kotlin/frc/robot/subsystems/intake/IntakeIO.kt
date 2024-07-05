94d22b649a9d788608ae42d74763d7e34d115af5
Emma Livne <emmalivne.03@gmail.com>
2024-07-05 13:35:30 +0300

    Add IntakeIO

---
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeIO.kt
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.intake
+
+import com.ctre.phoenix6.hardware.TalonFX
+
+interface IntakeIO {
+    fun setSpinPower() {
+
+    }
+    fun setCenterPower() {
+
+    }
+    fun setAngle() {
+
+    }
+}
