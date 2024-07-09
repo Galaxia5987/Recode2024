7292815de81c716bfab896860cc45acc5448e631
Emma Livne <emmalivne.03@gmail.com>
2024-07-09 18:38:49 +0300

    Add init and override functions

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeIOReal.kt
@@ -1,13 +1,43 @@
 package frc.robot.subsystems.intake
 
+import com.ctre.phoenix6.controls.PositionDutyCycle
+import com.ctre.phoenix6.controls.PositionVoltage
 import com.ctre.phoenix6.hardware.TalonFX
+import com.revrobotics.CANSparkBase
 import com.revrobotics.CANSparkLowLevel
 import com.revrobotics.CANSparkMax
+import edu.wpi.first.math.geometry.Rotation2d
 import frc.robot.Ports
 
 class IntakeIOReal : IntakeIO{
     val angleMotor = TalonFX(Ports.Intake.ANGLE_MOTOR_ID)
     val spinMotor = CANSparkMax(Ports.Intake.SPIN_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
     val centerMotor = CANSparkMax(Ports.Intake.CENTER_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
+    val positionControl = PositionVoltage(0.0)
 
+    init {
+        spinMotor.inverted = true
+        spinMotor.setSmartCurrentLimit(40)
+        spinMotor.idleMode = CANSparkBase.IdleMode.kCoast
+        spinMotor.enableVoltageCompensation(12.0)
+
+        centerMotor.inverted = true
+        centerMotor.setSmartCurrentLimit(40)
+        centerMotor.idleMode = CANSparkBase.IdleMode.kBrake
+        centerMotor.enableVoltageCompensation(12.0)
+
+//        angleMotor.configurator.apply(null) TODO: add configuration
+    }
+
+    override fun setSpinPower(power: Double) {
+        spinMotor.set(power)
+    }
+
+    override fun setCenterPower(power: Double) {
+        centerMotor.set(power)
+    }
+
+    override fun setAngle(angle: Rotation2d) {
+//        angleMotor.setControl(positionControl)
+    }
 }
