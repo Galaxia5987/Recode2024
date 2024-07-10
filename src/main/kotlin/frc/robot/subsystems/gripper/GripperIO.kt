9eaeaf3fe7223350a69e393a45f3195edca676c7
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-07-11 01:53:19 +0300

    Create GripperIO Interface

---
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperIO.kt
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.gripper
+
+import edu.wpi.first.units.MutableMeasure
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.Voltage
+import org.team9432.annotation.Logged
+
+interface GripperIO {
+    val inputs: LoggedGripperInputs
+        get() = LoggedGripperInputs()
+
+    fun setRollerMotorPower(power: Double) {}
+
+    fun updateInputs() {}
+
+    @Logged
+    open class GripperInputs {
+        var rollerPowerSetPoint = 0.0
+        var rollerMotorVoltage: MutableMeasure<Voltage> = Units.Volts.of(0.0).mutableCopy()
+        var hasNote = false
+    }
+}
