80e23eaf297afaacd6b0c39b002fa12b0ee66c30
Emma Livne <emmalivne.03@gmail.com>
2024-07-16 17:32:45 +0300

    Add Climb functions

--- src/main/kotlin/frc/robot/subsystems/climb/Climb.kt
+++ src/main/kotlin/frc/robot/subsystems/climb/Climb.kt
@@ -3,15 +3,36 @@ package frc.robot.subsystems.climb
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import java.util.function.DoubleSupplier
 
-class Climb(private val io: ClimbIO): SubsystemBase() {
+class Climb private constructor(private val io: ClimbIO) : SubsystemBase() {
     private val inputs = LoggedClimbInputs()
 
-    fun open():Command{
-        return Commands.run({io.closeStopper()}).until{io.isStopperStuck}
+    companion object {
+        @Volatile
+        private var instance: Climb? = null
+
+        fun initialize(io: ClimbIO){
+            synchronized(this){
+                if (instance==null)
+                    instance = Climb(io)
+            }
+        }
     }
 
-    fun lock():Command{
-        return Commands.run({io.closeStopper()}).until{io.isStopperStuck}
+    fun open(): Command {
+        return Commands.run({ io.closeStopper() })
+            .until { inputs.isStopperStuck }
+            .andThen({ io.disableStopper() })
+    }
+
+    fun lock(): Command {
+        return Commands.run({ io.closeStopper() })
+            .until { inputs.isStopperStuck }
+            .andThen({ io.disableStopper() })
+    }
+
+    fun setPower(power: DoubleSupplier): Command {
+        return Commands.run({ io.setPower(power.asDouble) })
     }
 }
