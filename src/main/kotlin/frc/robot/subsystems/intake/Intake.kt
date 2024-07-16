d04fd2252852ac7fc9ce6101cb0bf625d888d854
Gali2110 <galiworms@gmail.com>
2024-07-16 18:32:41 +0300

    Add io at Intake Constructor

--- src/main/kotlin/frc/robot/subsystems/intake/Intake.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/Intake.kt
@@ -1,6 +1,10 @@
 package frc.robot.subsystems.intake
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.littletonrobotics.junction.Logger
+
+class Intake(private val io: IntakeIO) : SubsystemBase() {
+    private val inputs = io.inputs
 
 class Intake : SubsystemBase() {
 
