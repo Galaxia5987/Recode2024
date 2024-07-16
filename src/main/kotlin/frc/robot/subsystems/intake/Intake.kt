450ced654ea4209006bdee578e351502ec59da25
Gali2110 <galiworms@gmail.com>
2024-07-16 18:33:32 +0300

    Implement all intake commands

--- src/main/kotlin/frc/robot/subsystems/intake/Intake.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/Intake.kt
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.intake
 
+import edu.wpi.first.units.Angle
+import edu.wpi.first.units.Measure
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.littletonrobotics.junction.Logger
 
@@ -13,4 +17,39 @@ class Intake(private val io: IntakeIO) : SubsystemBase() {
         )
     }
 
+    fun setCenterPower(power: Double): Command {
+        return Commands.run({
+                io.setCenterPower(power)
+            }
+        )
+    }
+
+    fun setAngle(angle: Measure<Angle>): Command {
+        return Commands.run({
+                inputs.angleSetPoint = angle
+                io.setAngle(angle)
+            }
+        )
+    }
+
+    fun intake(): Command {
+        return Commands.parallel(
+            setAngle(IntakeConstants.intakeAngle),
+            setSpinPower(IntakeConstants.intakeSpinPower),
+            setCenterPower(IntakeConstants.intakeCenterPower)
+        )
+    }
+
+    fun stop(): Command {
+        return Commands.parallel(
+            setAngle(IntakeConstants.restAngle),
+            setSpinPower(0.0),
+            setCenterPower(0.0)
+        )
+    }
+
+    override fun periodic() {
+        io.updateInputs()
+        Logger.processInputs(this::class.simpleName, inputs)
+    }
 }
