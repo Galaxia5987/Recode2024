9b0813053fff95c3168b9c338c39c9bb07f00d89
Gali2110 <galiworms@gmail.com>
2024-07-26 13:57:09 +0300

    Create VisionIO funs and inputs

--- src/main/kotlin/frc/robot/subsystems/vision/VisionIO.kt
+++ src/main/kotlin/frc/robot/subsystems/vision/VisionIO.kt
@@ -4,21 +4,20 @@ import edu.wpi.first.math.geometry.Pose3d
 import org.team9432.annotation.Logged
 
 interface VisionIO {
+    val inputs: LoggedVisionInputs
 
-    fun setPipeLine(pipeLineIndex: Int) {}
+    fun setPipeLine(pipeLineIndex: Int) {
 
-    fun getLastestResult() : VisionResult {
-        throw NotImplementedError("Method getLatestResult Not Implemented in current VisionIO")
     }
 
-    fun getName(): String = "Camera"
+    fun getLatestResult(): VisionResult? = null
 
     fun updateInputs() {}
-
 
     @Logged
     open class VisionInputs {
         var poseFieldOriented = Pose3d()
         var isConnected = false
+        var timestamp: Double = 0.0 // Seconds
     }
 }
