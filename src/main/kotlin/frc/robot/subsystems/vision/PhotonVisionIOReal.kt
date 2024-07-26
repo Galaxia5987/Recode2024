4c343802c53604dc87ca8962af718d1dd59e9517
Gali2110 <galiworms@gmail.com>
2024-07-26 13:56:06 +0300

    Create PhotonVisionIOReal

---
+++ src/main/kotlin/frc/robot/subsystems/vision/PhotonVisionIOReal.kt
@@ -0,0 +1,32 @@
+package frc.robot.subsystems.vision
+
+import edu.wpi.first.math.geometry.Transform3d
+import org.photonvision.PhotonCamera
+import org.photonvision.PhotonPoseEstimator
+
+class PhotonVisionIOReal(private val camera: PhotonCamera, private val robotToCam: Transform3d) : VisionIO {
+    override val inputs = LoggedVisionInputs()
+    private val estimator: PhotonPoseEstimator = PhotonPoseEstimator(VisionConstants.aprilTagFieldLayout, PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, camera, robotToCam)
+
+    init {
+        camera.pipelineIndex = 0
+    }
+
+    override fun setPipeLine(pipeLineIndex: Int) {
+        camera.pipelineIndex = pipeLineIndex
+    }
+
+    override fun getLatestResult(): VisionResult? {
+        // TODO: Implement Method
+    }
+
+    override fun updateInputs() {
+        inputs.isConnected = camera.isConnected
+
+        val latestResult = camera.latestResult
+        val estimatedPose = estimator.update(latestResult)
+
+        inputs.poseFieldOriented = estimatedPose.get().estimatedPose
+        inputs.timestamp = estimatedPose.get().timestampSeconds
+    }
+}
