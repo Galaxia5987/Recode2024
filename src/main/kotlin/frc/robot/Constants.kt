d5332e36017b4d82f85f1f8e5a75b8d179f7c0a6
Emma Livne <emmalivne.03@gmail.com>
2024-08-01 00:52:32 +0300

    Add init swerve function

--- src/main/kotlin/frc/robot/Constants.kt
+++ src/main/kotlin/frc/robot/Constants.kt
@@ -1,7 +1,10 @@
 package frc.robot
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration
+import com.ctre.phoenix6.configs.TalonFXConfiguration
 import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj.DriverStation.Alliance
+import frc.robot.subsystems.swerve.*
 
 object Constants {
     const val CONFIG_TIMEOUT: Int = 100 // [ms]
@@ -18,5 +21,36 @@ object Constants {
         val alliance = DriverStation.getAlliance()
         return if(alliance.isPresent) alliance.get() == Alliance.Red
         else false
+    }
+
+    fun initSwerve() {
+        var moduleIOs: Array<ModuleIO>
+
+        when(SwerveConstants.SWERVE_TYPE) {
+            SwerveConstants.SwerveType.SIM-> {
+                moduleIOs = Array<ModuleIO>(4) { ModuleIOSim() }
+                SwerveDrive.initialize(GyroIOSim(), SwerveConstants.OFFSETS, *moduleIOs)
+            }
+            SwerveConstants.SwerveType.WCP->{
+                moduleIOs = Array<ModuleIO>(4) {i->ModuleIOTalonFX(
+                    Ports.SwerveDriveWCP.DRIVE_IDS[i],
+                    Ports.SwerveDriveWCP.ANGLE_IDS[i],
+                    Ports.SwerveDriveWCP.ENCODER_IDS[i],
+                    SwerveConstants.DRIVE_MOTOR_CONFIGS ?: TalonFXConfiguration(), //TODO: Not sure if this is great cuz default config doesn't have current limits but it shouldn't be null so...
+                    SwerveConstants.ANGLE_MOTOR_CONFIGS ?: TalonFXConfiguration(),
+                    SwerveConstants.ENCODER_CONFIGS ?: CANcoderConfiguration(),
+                    SwerveConstants.OFFSETS[i]
+                )}
+            }
+            SwerveConstants.SwerveType.NEO->{
+                moduleIOs = Array<ModuleIO>(4) {i->ModuleIOSparkMax(
+                    Ports.SwerveDriveNEO.DRIVE_IDS[i],
+                    Ports.SwerveDriveNEO.ANGLE_IDS[i],
+                    Ports.SwerveDriveNEO.ENCODER_IDS[i],
+                    Ports.SwerveDriveNEO.DRIVE_INVERTED[i],
+                    Ports.SwerveDriveNEO.ANGLE_INVERTED[i]
+                )}
+            }
+        }
     }
 }
