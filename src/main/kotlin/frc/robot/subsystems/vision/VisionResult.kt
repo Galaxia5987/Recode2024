729ccfef26d180c9588ee9552fa3a0fac61b14ce
Gali2110 <galiworms@gmail.com>
2024-08-01 16:13:18 +0300

    Add distanceToTargets and poseAmbiguity to VisionResult

--- src/main/kotlin/frc/robot/subsystems/vision/VisionResult.kt
+++ src/main/kotlin/frc/robot/subsystems/vision/VisionResult.kt
@@ -1,10 +1,10 @@
 package frc.robot.subsystems.vision
 
 import edu.wpi.first.math.geometry.Pose3d
-import org.photonvision.EstimatedRobotPose
 
 data class VisionResult(
     var estimatedRobotPose: Pose3d,
     var timestamp: Double,
-    var ambiguity: Double
+    var distanceToTargets: MutableList<Double> = ArrayList(),
+    var poseAmbiguities: MutableList<Double> = ArrayList()
 )
