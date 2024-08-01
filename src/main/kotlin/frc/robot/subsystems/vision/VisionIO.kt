e5a60fa1288053e66d69a375558f8585f2dd160a
Gali2110 <galiworms@gmail.com>
2024-08-01 16:11:53 +0300

    Add distanceToTargets and poseAmbiguity to VisionIO

--- src/main/kotlin/frc/robot/subsystems/vision/VisionIO.kt
+++ src/main/kotlin/frc/robot/subsystems/vision/VisionIO.kt
@@ -19,6 +19,7 @@ interface VisionIO {
         var poseFieldOriented = Pose3d()
         var isConnected = false
         var timestamp: Double = 0.0 // Seconds
-        var ambiguity: Double = 0.0
+        var distanceToTargets: MutableList<Double> = ArrayList()
+        var poseAmbiguities: MutableList<Double> = ArrayList()
     }
 }
