08e5599f4a952ed2e2852e45f8c761b4bc8eea2a
Emma Livne <emmalivne.03@gmail.com>
2024-08-04 23:31:36 +0300

    Remove unnecessary constants

--- src/main/kotlin/frc/robot/subsystems/climb/ClimbConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/climb/ClimbConstants.kt
@@ -4,18 +4,12 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
-import edu.wpi.first.units.Angle
-import edu.wpi.first.units.MutableMeasure
-import edu.wpi.first.units.Units
 
 object ClimbConstants {
     const val GEAR_RATIO = 12.0
     const val STOPPER_MOTOR_CURRENT_LIMIT = 20
     const val STOPPER_MOTOR_CURRENT_THRESHOLD = 15 //TODO: calibrate
     val MOTOR_CONFIG = TalonFXConfiguration()
-
-    val OPEN_POSITION: MutableMeasure<Angle> = Units.Degree.of(40.0).mutableCopy()
-    val CLOSED_POSITION: MutableMeasure<Angle> = Units.Degree.of(140.0).mutableCopy()
 
     init {
         MOTOR_CONFIG.withMotorOutput(
