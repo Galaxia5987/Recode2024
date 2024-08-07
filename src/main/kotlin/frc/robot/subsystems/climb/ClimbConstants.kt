db0c4fc9c211256ae5829a34f1e6045ed951b576
Emma <emmalivne.03@gmail.com>
2024-08-07 20:03:11 +0300

    Add explanation to stopper current threshold constant

--- src/main/kotlin/frc/robot/subsystems/climb/ClimbConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/climb/ClimbConstants.kt
@@ -8,7 +8,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue
 object ClimbConstants {
     const val GEAR_RATIO = 12.0
     const val STOPPER_MOTOR_CURRENT_LIMIT = 20
-    const val STOPPER_MOTOR_CURRENT_THRESHOLD = 10
+    const val STOPPER_MOTOR_CURRENT_THRESHOLD = 10 //The current threshold for the stopper to stop at(when it hits something)
     const val STOPPER_MOTOR_VOLTAGE_COMPENSATION_SATURATION = 12.0
     const val STOPPER_MOTOR_POWER = 0.5
     val MOTOR_CONFIG = TalonFXConfiguration()
