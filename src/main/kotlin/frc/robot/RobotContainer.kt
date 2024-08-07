a43124a44bf564279f0a34333bd97f078b1a1260
Emma Livne <emmalivne.03@gmail.com>
2024-08-07 16:20:05 +0300

    Add buttons

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -3,10 +3,12 @@ package frc.robot
 
 import com.pathplanner.lib.auto.AutoBuilder
 import com.pathplanner.lib.auto.NamedCommands
+import edu.wpi.first.math.MathUtil
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
-import java.util.Optional
-import kotlin.math.absoluteValue
+import frc.robot.subsystems.climb.Climb
+import frc.robot.subsystems.climb.ClimbIOTalonFX
+import java.util.function.DoubleSupplier
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -20,18 +22,32 @@ object RobotContainer {
     private val testController = CommandXboxController(2)
     private val autoChooser = AutoBuilder.buildAutoChooser()
 
+    private val climb: Climb
+
     init {
+        Climb.initialize(ClimbIOTalonFX())
+        climb = Climb.getInstance()
         registerAutoCommands()
         configureButtonBindings()
         configureDefaultCommands()
     }
 
     private fun configureDefaultCommands() {
-
+        climb.setDefaultCommand(
+            climb.setPower(
+                DoubleSupplier {
+                    MathUtil.applyDeadband(
+                        -(driverController.leftTriggerAxis + 1) / 2
+                                + (driverController.rightTriggerAxis + 1) / 2,
+                        0.15
+                    )
+                })
+        )
     }
 
     private fun configureButtonBindings() {
-
+        driverController.start().onTrue(climb.lock())
+        driverController.back().onTrue(climb.open())
     }
 
     fun getAutonomousCommand(): Command = autoChooser.selected
