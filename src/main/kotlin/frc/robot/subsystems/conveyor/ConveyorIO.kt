fa00a88dec251d6802f5c34d20becb0a4b876afe
Emma Livne <emmalivne.03@gmail.com>
2024-08-10 20:36:11 +0300

    Add updatePID and hasPIDChanged functions

--- src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorIO.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorIO.kt
@@ -4,6 +4,7 @@ import edu.wpi.first.units.Angle
 import edu.wpi.first.units.MutableMeasure
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.Velocity
+import frc.robot.lib.webconstants.LoggedTunableNumber
 import org.team9432.annotation.Logged
 
 interface ConveyorIO {
@@ -14,6 +15,16 @@ interface ConveyorIO {
     fun updateInputs() {}
 
     fun stop() {}
+
+    fun updatePID() {}
+
+    fun hasPIDChanged(PIDValues: Array<LoggedTunableNumber>): Boolean {
+        var hasChanged = false
+        for (value in PIDValues) {
+            if (value.hasChanged()) hasChanged = true
+        }
+        return hasChanged
+    }
 
     @Logged
     open class ConveyorInputs {
