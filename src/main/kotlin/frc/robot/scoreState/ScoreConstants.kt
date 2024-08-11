d9ca24fbc6cc6ce3f67ba426a280030dc9a47426
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-08-11 17:44:45 +0300

    Create CONVEYOR_AMP_VELOCITY Constant

--- src/main/kotlin/frc/robot/scoreState/ScoreConstants.kt
+++ src/main/kotlin/frc/robot/scoreState/ScoreConstants.kt
@@ -4,4 +4,5 @@ import edu.wpi.first.math.geometry.Rotation2d
 
 object ScoreConstants {
     val AMP_ROTATION = Rotation2d.fromDegrees(-90.0)
+    val CONVEYOR_AMP_VELOCITY: Measure<Velocity<Angle>> = Units.RotationsPerSecond.of(15.0)
 }
