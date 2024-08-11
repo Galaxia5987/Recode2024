89032e1ad87fb1bd2e9b9c220558e117db587da1
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-08-11 17:43:23 +0300

    Add INTAKE_POWER constants

--- src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
@@ -1,11 +1,12 @@
 package frc.robot.subsystems.gripper
 
-import com.ctre.phoenix6.signals.InvertedValue
 import edu.wpi.first.units.Current
 import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 
 object GripperConstants {
     const val ROLLER_INVERTED_VALUE = true
+    const val INTAKE_POWER = 0.7
+
     val currentLimit : Measure<Current> = Units.Amp.of(40.0)
 }
