11babe621ec7eb6c2a720d85fa82150617aeb9e9
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-08-11 19:12:59 +0300

    Update PoseEstimation.kt

--- src/main/kotlin/frc/robot/lib/PoseEstimation.kt
+++ src/main/kotlin/frc/robot/lib/PoseEstimation.kt
@@ -1,13 +1,18 @@
 package frc.robot.lib
 
+import edu.wpi.first.math.VecBuilder
+import frc.robot.subsystems.swerve.SwerveConstants
+import frc.robot.subsystems.swerve.SwerveDrive
 import frc.robot.subsystems.vision.Vision
+import java.util.stream.DoubleStream
 
 class PoseEstimation {
     private val vision: Vision = Vision.getInstance()
+    private val swerveDrive: SwerveDrive = SwerveDrive.getInstance()
 
     companion object {
         @Volatile
-        private var instance: PoseEstimation ?= null
+        private var instance: PoseEstimation? = null
 
         fun initialize() {
             synchronized(this) {
@@ -19,8 +24,19 @@ class PoseEstimation {
 
         fun getInstance(): PoseEstimation {
             return instance ?: throw IllegalArgumentException(
-                "PoseEst has not been initialized. Call initialize() first."
+                "PoseEstimation has not been initialized. Call initialize() first."
             )
+        }
+
+        /**
+         * Averages ambiguity of estimated poses using a harmonic average. Can be from different targets
+         * in vision module, or between module.
+         *
+         * @param ambiguities the ambiguities to average.
+         * @return the average of the ambiguities.
+         */
+        fun averageAmbiguity(ambiguities: List<Double>): Double {
+            return ambiguities.size / ambiguities.sumOf { 1.0 / it }
         }
     }
 
@@ -31,7 +47,15 @@ class PoseEstimation {
             val ambiguities = result.distanceToTargets.map { d -> d * d }
             val stddev = multiplier //* ambiguities // TODO: Do Harmonic Average for ambiguities list before calculating stddev
 
-            // TODO: Update Swerve Drive Estimator with Vision Measurements
+            swerveDrive.estimator.addVisionMeasurement(
+                result.estimatedRobotPose.toPose2d(),
+                result.timestamp,
+                VecBuilder.fill(
+                    stddev,
+                    stddev,
+                    stddev * SwerveConstants.MAX_OMEGA_VELOCITY / SwerveConstants.MAX_X_Y_VELOCITY
+                )
+            )
         }
     }
 }
