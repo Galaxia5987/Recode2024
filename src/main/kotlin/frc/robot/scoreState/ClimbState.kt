1859a51b60655853a1dc6b9165a47b6cfa1624df
Emma Livne <emmalivne.03@gmail.com>
2024-08-12 01:31:47 +0300

    Add climb state

---
+++ src/main/kotlin/frc/robot/scoreState/ClimbState.kt
@@ -0,0 +1,55 @@
+package frc.robot.scoreState
+
+import com.pathplanner.lib.auto.AutoBuilder
+import com.pathplanner.lib.path.PathConstraints
+import com.pathplanner.lib.util.GeometryUtil
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.units.Units
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.StartEndCommand
+import frc.robot.Constants
+import frc.robot.commandGroups.CommandGroups
+import frc.robot.subsystems.climb.Climb
+import frc.robot.subsystems.swerve.SwerveDrive
+
+class ClimbState: ScoreState {
+
+    private val swerveDrive = SwerveDrive.getInstance()
+    private val botPose = Pose2d() //Todo: replace with estimator
+
+    private fun getNearestChain(): Pose2d{
+        return when (Constants.alliance){
+            Constants.Alliance.RED -> {
+                val chainLocations = List<Pose2d>(ScoreConstants.chainLocations.size)
+                {i -> GeometryUtil.flipFieldPose(ScoreConstants.chainLocations[i])}
+                botPose.nearest(chainLocations)
+            }
+            Constants.Alliance.BLUE ->
+                botPose.nearest(ScoreConstants.chainLocations.asList())
+        }
+    }
+
+    private fun pathFindToPose(pose: Pose2d): Command{
+        return Commands.parallel(
+            AutoBuilder.pathfindToPose(pose, Constants.PATH_CONSTRAINTS),
+            swerveDrive.turnCommand(
+                Units.Rotations.of(
+                getNearestChain().rotation.rotations).mutableCopy(),
+                2.0/360
+            )
+        )
+    }
+
+    override fun execute(): Command {
+        val optimalPose: Pose2d = getNearestChain()
+        return StartEndCommand(
+            {Commands.sequence(
+                CommandGroups.openClimb(),
+                pathFindToPose(optimalPose),
+                CommandGroups.closeClimb()
+            )},
+            Commands::none, //TODO: Replace with LEDs command
+            swerveDrive, Climb.getInstance())
+    }
+}
