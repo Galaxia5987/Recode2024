e3cd3e0268212f2c3eca16585241bdb0b18f16b7
Emma Livne <emmalivne.03@gmail.com>
2024-08-12 01:31:26 +0300

    Add chain locations constant

--- src/main/kotlin/frc/robot/scoreState/ScoreConstants.kt
+++ src/main/kotlin/frc/robot/scoreState/ScoreConstants.kt
@@ -1,8 +1,12 @@
 package frc.robot.scoreState
 
+import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.units.*
 
 object ScoreConstants {
+
+    val chainLocations = arrayOf(Pose2d(), Pose2d(), Pose2d()) //left, right, middle
+
     val AMP_ROTATION: Measure<Angle> = Units.Degrees.of(-90.0)
     val SHOOTER_TOP_AMP_VELOCITY: Measure<Velocity<Angle>> = Units.RotationsPerSecond.of(8.5)
     val SHOOTER_BOTTOM_AMP_VELOCITY: Measure<Velocity<Angle>> = Units.RotationsPerSecond.of(12.5)
