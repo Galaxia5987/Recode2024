40f9e5d7280404e62c2eb8cd8285fa5e5e1b149a
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-08-11 19:13:29 +0300

    Remove unused functions from Utils

--- src/main/kotlin/frc/robot/lib/Utils.java
+++ src/main/kotlin/frc/robot/lib/Utils.java
@@ -21,28 +21,6 @@ public class Utils {
         return Math.abs(a - b) <= maxError;
     }
 
-    public static boolean speedsEpsilonEquals(ChassisSpeeds speeds) {
-        return epsilonEquals(speeds.vxMetersPerSecond, 0)
-                && epsilonEquals(speeds.vyMetersPerSecond, 0)
-                && epsilonEquals(speeds.omegaRadiansPerSecond, 0);
-    }
-
-    public static Pose3d pose2dToPose3d(Pose2d pose) {
-        return new Pose3d(
-                pose.getX(), pose.getY(), 0, new Rotation3d(0, 0, pose.getRotation().getRadians()));
-    }
-
-    /**
-     * Averages ambiguity of estimated poses using a harmonic average. Can be from different targets
-     * in vision module, or between module.
-     *
-     * @param ambiguities the ambiguities to average.
-     * @return the average of the ambiguities.
-     */
-    public static double averageAmbiguity(List<Double> ambiguities) {
-        return 1.0 / ambiguities.stream().map((num) -> 1.0 / num).reduce(0.0, Double::sum);
-    }
-
     public static double normalize(double angleRadians) {
         while (angleRadians < 0) {
             angleRadians += 2 * Math.PI;
