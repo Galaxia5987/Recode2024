d6a803e853a2f50b15442bd6e4c10f528d7f4790
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-08-11 16:41:29 +0300

    Create AmpState

---
+++ src/main/kotlin/frc/robot/scoreState/AmpState.kt
@@ -0,0 +1,9 @@
+package frc.robot.scoreState
+
+import edu.wpi.first.wpilibj2.command.Command
+
+class AmpState : ScoreState {
+    override fun execute(): Command {
+        TODO("Not yet implemented")
+    }
+}
