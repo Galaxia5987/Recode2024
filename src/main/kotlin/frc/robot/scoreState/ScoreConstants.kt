ece29723c21017a3df06f46acf08a0b0dd579528
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-08-11 17:45:02 +0300

    Create Shooter AMP Constants

--- src/main/kotlin/frc/robot/scoreState/ScoreConstants.kt
+++ src/main/kotlin/frc/robot/scoreState/ScoreConstants.kt
@@ -1,8 +1,10 @@
 package frc.robot.scoreState
 
-import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.units.*
 
 object ScoreConstants {
     val AMP_ROTATION = Rotation2d.fromDegrees(-90.0)
+    val SHOOTER_TOP_AMP_VELOCITY: Measure<Velocity<Angle>> = Units.RotationsPerSecond.of(8.5)
+    val SHOOTER_BOTTOM_AMP_VELOCITY: Measure<Velocity<Angle>> = Units.RotationsPerSecond.of(12.5)
     val CONVEYOR_AMP_VELOCITY: Measure<Velocity<Angle>> = Units.RotationsPerSecond.of(15.0)
 }
