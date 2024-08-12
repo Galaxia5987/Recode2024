683ea7307f1d3d3104dc4eb9c3c927c2549122f5
Emma Livne <emmalivne.03@gmail.com>
2024-08-12 03:14:41 +0300

    Add speaker pose constant

--- src/main/kotlin/frc/robot/scoreState/ScoreConstants.kt
+++ src/main/kotlin/frc/robot/scoreState/ScoreConstants.kt
@@ -1,7 +1,11 @@
 package frc.robot.scoreState
 
 import edu.wpi.first.math.geometry.Pose2d
-import edu.wpi.first.units.*
+import edu.wpi.first.math.geometry.Translation2d
+import edu.wpi.first.units.Angle
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.Velocity
 
 object ScoreConstants {
 
@@ -11,4 +15,6 @@ object ScoreConstants {
     val SHOOTER_TOP_AMP_VELOCITY: Measure<Velocity<Angle>> = Units.RotationsPerSecond.of(8.5)
     val SHOOTER_BOTTOM_AMP_VELOCITY: Measure<Velocity<Angle>> = Units.RotationsPerSecond.of(12.5)
     val CONVEYOR_AMP_VELOCITY: Measure<Velocity<Angle>> = Units.RotationsPerSecond.of(15.0)
+
+    val SPEAKER_POSE_BLUE = Translation2d(0.0, 5.5479442)
 }
