36056711ff458f22a749f6150fd1f2c944d9e56e
Emma Livne <emmalivne.03@gmail.com>
2024-08-12 03:15:08 +0300

    Add shootState

---
+++ src/main/kotlin/frc/robot/scoreState/ShootState.kt
@@ -0,0 +1,107 @@
+package frc.robot.scoreState
+
+import com.pathplanner.lib.util.GeometryUtil
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.units.Distance
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.Units
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.StartEndCommand
+import frc.robot.Constants
+import frc.robot.commandGroups.CommandGroups
+import frc.robot.lib.extensions.TranslationExtensions.getRotationToTranslation
+import frc.robot.subsystems.conveyor.Conveyor
+import frc.robot.subsystems.gripper.Gripper
+import frc.robot.subsystems.hood.Hood
+import frc.robot.subsystems.shooter.Shooter
+import frc.robot.subsystems.swerve.SwerveDrive
+
+class ShootState : ScoreState {
+    private val swerveDrive = SwerveDrive.getInstance()
+    private val shooter = Shooter.getInstance()
+    private val hood = Hood.getInstance()
+    private val conveyor = Conveyor.getInstance()
+    private val gripper = Gripper.getInstance()
+
+    val botPose = Pose2d()
+
+    fun getDistanceToSpeaker(): Measure<Distance> {
+        return when (Constants.alliance) {
+            Constants.Alliance.RED -> Units.Meters.of(
+                botPose.translation.getDistance(
+                    GeometryUtil.flipFieldPosition(
+                        ScoreConstants.SPEAKER_POSE_BLUE
+                    )
+                )
+            )
+
+            Constants.Alliance.BLUE -> Units.Meters.of(
+                botPose.translation.getDistance(
+                    ScoreConstants.SPEAKER_POSE_BLUE
+                )
+            )
+        }
+    }
+
+    fun getRotationToSpeaker(): Rotation2d {
+        return when (Constants.alliance) {
+            Constants.Alliance.RED ->
+                botPose.translation.getRotationToTranslation(
+                    GeometryUtil.flipFieldPosition(
+                        ScoreConstants.SPEAKER_POSE_BLUE
+                    )
+                )
+
+            Constants.Alliance.BLUE ->
+                botPose.translation.getRotationToTranslation(
+                    ScoreConstants.SPEAKER_POSE_BLUE
+                )
+        }
+    }
+
+    fun warmup(distanceToSpeaker: Measure<Distance>): Command {
+        return Commands.none()//TODO: Add CSV files and stuff and implement interpolation
+    }
+
+    fun turnToSpeaker(): Command {
+        return swerveDrive.turnCommand(
+            Units.Rotations.of(
+                getRotationToSpeaker().rotations
+            ).mutableCopy(),
+            3.0 / 360.0
+        )
+    }
+
+    fun readyToShoot(): Boolean {
+        return shooter.atSetpoint() && hood.atSetpoint()
+    }
+
+    fun init(): Runnable {
+        return Runnable {
+            Commands.parallel(
+                warmup(getDistanceToSpeaker()),
+                turnToSpeaker()
+            ).until(::readyToShoot).andThen(Commands.none()) //TODO: Replace with LEDs command
+        }
+    }
+
+    fun end(): Runnable {
+        return Runnable {
+            gripper.feed()
+                .andThen(
+                    CommandGroups.stopWarmup(),
+                    Commands.none() //TODO: Replace with LEDs command
+                )
+        }
+    }
+
+    override fun execute(): Command {
+        return StartEndCommand(
+            init(),
+            end(),
+            swerveDrive, shooter, hood, conveyor, gripper
+        )
+    }
+}
