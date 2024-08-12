2645a8a75ba5f45d7116538ebf230eb0874e3528
Emma Livne <emmalivne.03@gmail.com>
2024-08-12 20:47:02 +0300

    Turn the gripper motor's inversion off

--- src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
@@ -5,7 +5,7 @@ import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 
 object GripperConstants {
-    const val ROLLER_INVERTED_VALUE = true
+    const val ROLLER_INVERTED_VALUE = false
     const val INTAKE_POWER = 0.7
 
     val currentLimit : Measure<Current> = Units.Amp.of(40.0)
