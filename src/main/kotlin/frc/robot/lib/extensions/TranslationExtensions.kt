f96ac39d281f55c8f416171fdde9644a22e2bbc2
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-08-19 23:17:59 +0300

    Refactor getRotationToTranslation Extension function to be more concise

--- src/main/kotlin/frc/robot/lib/extensions/TranslationExtensions.kt
+++ src/main/kotlin/frc/robot/lib/extensions/TranslationExtensions.kt
@@ -5,10 +5,5 @@ import edu.wpi.first.math.geometry.Translation2d
 
 object TranslationExtensions {
 
-    fun Translation2d.getRotationToTranslation(other: Translation2d): Rotation2d {
-        return Rotation2d(
-            other.x - this.x,
-            other.y - this.y
-        )
-    }
+    fun Translation2d.getRotationToTranslation(other: Translation2d): Rotation2d = (this - other).angle
 }
