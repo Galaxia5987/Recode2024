207646fb89237d8c084e28c56dbbc71a7efed800
Emma Livne <emmalivne.03@gmail.com>
2024-08-23 12:37:43 +0300

    Update hood offset

--- src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
@@ -49,7 +49,7 @@ object HoodConstants {
         kV.initDefault(0.0)
         kA.initDefault(0.0)
         kG.initDefault(9.0)
-        ABSOLUTE_ENCODER_OFFSET.initDefault((57.832 - 33.48) / 360.0)
+        ABSOLUTE_ENCODER_OFFSET.initDefault((58.095 - 33.48) / 360.0)
         MOTOR_CONFIGURATION
             .withFeedback(FeedbackConfigs().withSensorToMechanismRatio(GEAR_RATIO))
             .withMotionMagic(
