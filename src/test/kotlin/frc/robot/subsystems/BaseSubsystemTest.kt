28a95658aee8ca8e8e2b76dc83e73c388c18a1a8
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-08-23 12:22:31 +0300

    Create BaseSubsystemTest

---
+++ src/test/kotlin/frc/robot/subsystems/BaseSubsystemTest.kt
@@ -0,0 +1,35 @@
+package frc.robot.subsystems
+
+import edu.wpi.first.hal.HAL
+import edu.wpi.first.wpilibj2.command.CommandScheduler
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+
+abstract class BaseSubsystemTest {
+    private lateinit var schedulerExecutor: ScheduledExecutorService
+
+    @BeforeEach
+    fun setUp() {
+        assert(HAL.initialize(500, 0)) { "HAL Initialization Failed" }
+
+        schedulerExecutor = Executors.newScheduledThreadPool(1)
+        schedulerExecutor.scheduleAtFixedRate(
+            { CommandScheduler.getInstance().run() }, 0, 20, TimeUnit.MILLISECONDS
+        )
+    }
+
+    @AfterEach
+    fun tearDown() {
+        schedulerExecutor.shutdown()
+        try {
+            if (!schedulerExecutor.awaitTermination(1, TimeUnit.SECONDS)) {
+                schedulerExecutor.shutdownNow()
+            }
+        } catch (e: InterruptedException) {
+            schedulerExecutor.shutdownNow()
+        }
+    }
+}
