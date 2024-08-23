90dd4b36ce35116afcfed0db6dfef1ed0964475f
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-08-23 12:23:15 +0300

    Refactor ShooterTest to use Abstract class

--- src/test/kotlin/frc/robot/subsystems/shooter/ShooterTest.kt
+++ src/test/kotlin/frc/robot/subsystems/shooter/ShooterTest.kt
@@ -1,33 +1,21 @@
 package frc.robot.subsystems.shooter
 
-import edu.wpi.first.hal.HAL
 import edu.wpi.first.units.Units
 import edu.wpi.first.wpilibj.simulation.SimHooks
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.CommandScheduler
-import org.junit.jupiter.api.AfterEach
+import frc.robot.subsystems.BaseSubsystemTest
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.util.concurrent.Executors
-import java.util.concurrent.ScheduledExecutorService
-import java.util.concurrent.TimeUnit
 
-class ShooterTest {
+class ShooterTest : BaseSubsystemTest() {
     private lateinit var shooter: Shooter
-    private lateinit var schedulerExecutor: ScheduledExecutorService
 
     @BeforeEach
-    fun setUp() {
-        assert(HAL.initialize(500, 0)) { "HAL Initialization Failed" }
+    fun initializeShooter() {
         val shooterIO = ShooterIOSim()
         Shooter.initialize(shooterIO)
         shooter = Shooter.getInstance()
-
-        schedulerExecutor = Executors.newScheduledThreadPool(1)
-        schedulerExecutor.scheduleAtFixedRate(
-            { CommandScheduler.getInstance().run() }, 0, 20, TimeUnit.MILLISECONDS
-        )
     }
 
     @Test
@@ -42,19 +30,6 @@ class ShooterTest {
 
         Thread.sleep(1000)
 
-        println()
         assertTrue(shooter.atSetpoint())
-    }
-
-    @AfterEach
-    fun tearDown() {
-        schedulerExecutor.shutdown()
-        try {
-            if (!schedulerExecutor.awaitTermination(1, TimeUnit.SECONDS)) {
-                schedulerExecutor.shutdownNow()
-            }
-        } catch (e: InterruptedException) {
-            schedulerExecutor.shutdownNow()
-        }
     }
 }
