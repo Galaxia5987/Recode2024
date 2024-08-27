37a1e132a47d69481c670fbfb8fa5bc04fe12bd2
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-08-27 15:45:43 +0300

    Create Button Bindings for ScoreState

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -1,12 +1,13 @@
 package frc.robot
 
-import com.pathplanner.lib.auto.AutoBuilder
 import com.pathplanner.lib.auto.NamedCommands
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
-import frc.robot.subsystems.swerve.SwerveDrive
+import frc.robot.scoreState.AmpState
+import frc.robot.scoreState.ClimbState
+import frc.robot.scoreState.ScoreState
+import frc.robot.scoreState.ShootState
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -20,11 +21,18 @@ object RobotContainer {
     private val testController = CommandXboxController(2)
 
     private val autoChooser: SendableChooser<Command> = AutoBuilder.buildAutoChooser()
+    private val shootState: ShootState by lazy { ShootState() }
+    private val ampState: AmpState by lazy { AmpState() }
+    private val climbState: ClimbState by lazy { ClimbState() }
+
+    private lateinit var currentState: ScoreState
 
     init {
         registerAutoCommands()
         configureButtonBindings()
         configureDefaultCommands()
+
+        currentState = shootState
     }
 
     private fun configureDefaultCommands() {
@@ -36,7 +44,10 @@ object RobotContainer {
     }
 
     private fun configureButtonBindings() {
-        ControllerInputs.getDriverController().y().onTrue(Commands.runOnce({ swerveDrive.resetGyro() }))
+        ControllerInputs.operatorController().a().whileTrue(currentState.execute())
+        ControllerInputs.operatorController().b().onTrue(Commands.runOnce({ currentState = shootState }))
+        ControllerInputs.operatorController().x().onTrue(Commands.runOnce({ currentState = ampState }))
+        ControllerInputs.operatorController().y().onTrue(Commands.runOnce({ currentState = climbState }))
     }
 
     fun getAutonomousCommand(): Command = Commands.none()
