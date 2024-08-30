641828be7943dc764b69b8a4a8a6ff2e5d7e958c
yahali david mor <mor.yahali1612@gmail.com>
2024-08-30 12:17:46 +0300

    Create ClimbIOReal.kt

---
+++ src/main/kotlin/frc/robot/subsystems/climb/ClimbIOReal.kt
@@ -0,0 +1,42 @@
+package frc.robot.subsystems.climb
+
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
+import com.ctre.phoenix6.hardware.TalonFX
+import frc.robot.Ports
+
+class ClimbIOReal:ClimbIO{
+    override val inputs = LoggedClimbInputs()
+
+    private val mainClimbMotor:TalonFX = TalonFX(Ports.Climb.MAIN_MOTOR_ID)
+    private val auxClimbMotor:TalonFX = TalonFX(Ports.Climb.AUX_MOTOR_ID)
+    private val lockMotor:TalonSRX = TalonSRX(Ports.Climb.STOPPER_ID)
+
+    init {
+        mainClimbMotor.configurator.apply(ClimbConstants.MOTOR_CONFIG)
+        auxClimbMotor.configurator.apply(ClimbConstants.MOTOR_CONFIG)
+    }
+
+    override fun updateInput() {
+        inputs.climbMotorVoltage = mainClimbMotor.supplyVoltage.value
+    }
+
+
+    override fun setPower(power: Double) {
+        mainClimbMotor.set(power)
+        auxClimbMotor.set(power)
+    }
+
+    override fun lockClimb() {
+        lockMotor.set(TalonSRXControlMode.PercentOutput,ClimbConstants.STOPPER_MOTOR_POWER)
+    }
+
+    override fun unlockClimb() {
+        lockMotor.set(TalonSRXControlMode.PercentOutput,-ClimbConstants.STOPPER_MOTOR_POWER)
+    }
+
+    override fun disableLockMotor() {
+        lockMotor.neutralOutput()
+    }
+
+}
