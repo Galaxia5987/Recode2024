25a92dc87a0e22f77f581483795f2d7926824eaf
yahali david mor <mor.yahali1612@gmail.com>
2024-08-30 12:17:48 +0300

    Update Climb.kt

--- src/main/kotlin/frc/robot/subsystems/climb/Climb.kt
+++ src/main/kotlin/frc/robot/subsystems/climb/Climb.kt
@@ -1,65 +1,48 @@
 package frc.robot.subsystems.climb
 
-import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import org.littletonrobotics.junction.AutoLogOutput
-import org.littletonrobotics.junction.Logger
 import java.util.function.DoubleSupplier
+import java.util.function.Supplier
 import kotlin.math.absoluteValue
 
-class Climb private constructor(private val io: ClimbIO) : SubsystemBase() {
-    private val inputs = io.inputs
-    private val timer = Timer()
+class Climb private constructor(private val io:ClimbIO) :SubsystemBase() {
+    private var input: LoggedClimbInputs = io.inputs
 
-    @AutoLogOutput
-    private var isStopperStuck = false
+    private var isStopperStuck: () -> Boolean = {false}
 
-    companion object {
+    companion object{
         @Volatile
         private var instance: Climb? = null
 
-        fun initialize(io: ClimbIO) {
-            synchronized(this) {
-                if (instance == null)
+        fun initialize(io: ClimbIO){
+            synchronized(this){
+                if(instance == null){
                     instance = Climb(io)
+                }
             }
         }
-
         fun getInstance(): Climb {
-            return instance ?: throw IllegalStateException("climb not initialized, call initialize(io)")
+            return instance ?: throw  IllegalStateException(
+                "Climb has not been initialized. Call initialize(io: IntakeIO) first."
+            )
         }
     }
-
-    init {
-        timer.start()
-        timer.reset()
+    fun setPower(power:DoubleSupplier):Command{
+        return Commands.run({io.setPower(power.asDouble)})
     }
 
-    fun open(): Command {
-        return Commands.run(io::openStopper)
-            .until { isStopperStuck }
-            .andThen(io::disableStopper)
+    fun lock():Command{
+        return Commands.run({io.lockClimb()}).until(isStopperStuck).andThen({io.disableLockMotor()})
     }
 
-    fun lock(): Command {
-        return Commands.run(io::closeStopper)
-            .until { isStopperStuck }
-            .andThen(io::disableStopper)
-    }
 
-    fun setPower(power: DoubleSupplier): Command {
-        return run { io.setPower(power.asDouble) }
+    fun unlock():Command{
+        return Commands.run({io.unlockClimb()}).until(isStopperStuck).andThen({io.disableLockMotor()})
     }
 
     override fun periodic() {
-        isStopperStuck =
-            inputs.stopperCurrent.absoluteValue >=
-                    ClimbConstants.STOPPER_MOTOR_CURRENT_THRESHOLD.absoluteValue
-        io.updateInputs()
-        if (timer.advanceIfElapsed(0.1)) {
-            Logger.processInputs(this::class.simpleName, inputs)
-        }
+        super.periodic()
     }
 }
