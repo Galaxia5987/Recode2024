c547fe2faf74487e418c6f190b09caa368bb510f
yahali david mor <mor.yahali1612@gmail.com>
2024-09-06 13:16:46 +0300

    Update Hood.kt

--- src/main/kotlin/frc/robot/subsystems/hood/Hood.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/Hood.kt
@@ -1,84 +1,35 @@
 package frc.robot.subsystems.hood
 
-import edu.wpi.first.math.geometry.Pose3d
-import edu.wpi.first.math.geometry.Rotation3d
-import edu.wpi.first.units.Angle
-import edu.wpi.first.units.MutableMeasure
-import edu.wpi.first.units.Units
-import edu.wpi.first.wpilibj.Timer
-import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d
-import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import org.littletonrobotics.junction.AutoLogOutput
-import org.littletonrobotics.junction.Logger
 
-class Hood private constructor(private val io: HoodIO) : SubsystemBase() {
-    private val inputs: LoggedHoodInputs = io.inputs
-    @AutoLogOutput
-    private var angleSetpoint: MutableMeasure<Angle> = MutableMeasure.zero(Units.Rotations)
-    private val timer = Timer()
-    private val encoderTimer = Timer()
+class Hood private constructor(private var io: HoodIO):SubsystemBase(){
+    private val inputs = io.inputs
 
-    @AutoLogOutput
-    private val mechanism2d = Mechanism2d(HoodConstants.SIMULATION_LENGTH, HoodConstants.SIMULATION_LENGTH)
-    private val root = mechanism2d.getRoot("Hood", HoodConstants.MECHANISM_2D_POSE.x, HoodConstants.MECHANISM_2D_POSE.y)
-    private val hood = root.append(
-        MechanismLigament2d(
-            "Hood", HoodConstants.HOOD_LENGTH.`in`(Units.Meters), 45.0
-        )
-    )
-
-    companion object { // Custom Singleton Implementation
+    companion object{
         @Volatile
-        private var instance: Hood? = null
+        private var instance:Hood? = null
 
-        fun initialize(io: HoodIO) {
-            synchronized(this) {
-                if (instance == null) {
+        fun initialize(io:HoodIO){
+            synchronized(this){
+                if(instance == null){
                     instance = Hood(io)
                 }
             }
         }
-
-        fun getInstance(): Hood {
-            return instance ?: throw IllegalStateException(
+        fun getInstance():Hood{
+            return instance?:throw IllegalStateException(
                 "Hood has not been initialized. Call initialize(io: HoodIO) first."
             )
         }
     }
+    fun setAngle(angle:Double):Command = Commands.run({io.setAngle(angle)}).withName("set Angle Hood")
 
-    init {
-        timer.start()
-        timer.reset()
+    fun setRestAngle():Command = Commands.run({io.setAngle(HoodConstants.restAngle)})
 
-        encoderTimer.start()
-        encoderTimer.reset()
-    }
+    fun isNear():Command = Commands.run({
+        inputs.angle
+    })
 
-    @AutoLogOutput
-    fun atSetpoint(): Boolean =
-        inputs.absoluteEncoderAngle.isNear(angleSetpoint, HoodConstants.MAX_TOLERANCE)
-
-    fun getAngle(): MutableMeasure<Angle> = inputs.internalAngle
-
-    fun setAngle(angle: MutableMeasure<Angle>): Command {
-        return run {
-            angleSetpoint.mut_replace(angle)
-            io.setAngle(angle)
-        }.withName("Set Angle Hood")
-    }
-
-    fun setRestingAngle(): Command = setAngle(HoodConstants.RESTING_ANGLE.mutableCopy()).withName("Set Resting Angle Hood")
-
-    @AutoLogOutput(key = "Hood/Pose")
-    private fun getPose3d() : Pose3d = Pose3d(HoodConstants.ROOT_POSITION, Rotation3d(0.0, getAngle().plus(HoodConstants.SIMULATION_OFFSET).`in`(Units.Radians), 0.0))
-
-    override fun periodic() {
-        io.updateInputs()
-        if (encoderTimer.advanceIfElapsed(0.5)) io.updateInternalEncoder()
-        Logger.processInputs("Hood", inputs)
-
-        hood.setAngle(inputs.internalAngle.`in`(Units.Degrees))
-    }
 }
