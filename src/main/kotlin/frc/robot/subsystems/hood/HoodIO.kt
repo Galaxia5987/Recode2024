84958d9cf06fb17ec05d71d024ddd49b9efeba81
yahali david mor <mor.yahali1612@gmail.com>
2024-09-06 13:23:17 +0300

    Update HoodIO.kt

--- src/main/kotlin/frc/robot/subsystems/hood/HoodIO.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodIO.kt
@@ -2,6 +2,8 @@ package frc.robot.subsystems.hood
 
 import edu.wpi.first.units.Angle
 import edu.wpi.first.units.MutableMeasure
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.Voltage
 import org.team9432.annotation.Logged
 
 interface HoodIO {
@@ -13,8 +15,9 @@ interface HoodIO {
 
     @Logged
     open class InputHood{
-        var angle:MutableMeasure<Angle> =
-        var angleMotorVoltage = 0.0
-        var encoderPosition = 0.0
+        var angle:MutableMeasure<Angle> = MutableMeasure.zero(Units.Rotations)
+        var angleSetPoint:MutableMeasure<Angle> = MutableMeasure.zero(Units.Rotations)
+        var angleMotorVoltage:MutableMeasure<Voltage> = MutableMeasure.zero(Units.Volts)
+        var encoderPosition:MutableMeasure<Angle> = MutableMeasure.zero(Units.Rotations)
     }
 }
