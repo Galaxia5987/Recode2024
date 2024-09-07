1f50d1b52878b3f99fff180699bb89bddfe154c1
Emma Livne <emmalivne.03@gmail.com>
2024-09-07 15:28:04 +0300

    Update hood offset

--- src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
@@ -29,7 +29,7 @@ object HoodConstants {
     val INVERTED_VALUE = InvertedValue.CounterClockwise_Positive
 
 
-    val ABSOLUTE_ENCODER_OFFSET = LoggedTunableNumber("Hood/EncoderOffset", (58.095 - 33.48) / 360.0)
+    val ABSOLUTE_ENCODER_OFFSET = LoggedTunableNumber("Hood/EncoderOffset", (-224.5 - 33.48) / 360.0)
 
     val GAINS by lazy {
         selectGainsBasedOnMode(
