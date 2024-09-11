d769bf24071463d1da853ef57b3091699461ce6a
yahali david mor <mor.yahali1612@gmail.com>
2024-09-11 18:50:40 +0300

    update Conveyor.kt

--- src/main/kotlin/frc/robot/subsystems/conveyor/Conveyor.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/Conveyor.kt
@@ -1,65 +1,36 @@
 package frc.robot.subsystems.conveyor
 
-import edu.wpi.first.units.Angle
-import edu.wpi.first.units.MutableMeasure
-import edu.wpi.first.units.Units
-import edu.wpi.first.units.Velocity
-import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import org.littletonrobotics.junction.AutoLogOutput
 import org.littletonrobotics.junction.Logger
 
-class Conveyor private constructor(private val io: ConveyorIO) : SubsystemBase() {
-    @AutoLogOutput
-    private var velocitySetpoint: MutableMeasure<Velocity<Angle>> = MutableMeasure.zero(Units.RotationsPerSecond)
-    private val inputs = io.inputs
-    private val timer = Timer()
+class Conveyor private constructor(private val io:ConveyorIO):SubsystemBase() {
+    private var inputs = io.inputs
+    companion object{
+        @Volatile
+        private var instance:Conveyor? = null
 
-    companion object {
-        private var instance: Conveyor? = null
-
-        fun initialize(io: ConveyorIO) {
-            synchronized(this) {
-                if (instance == null) {
+        fun initialize(io: ConveyorIO){
+            synchronized(this){
+                if(instance == null){
                     instance = Conveyor(io)
                 }
             }
         }
-
-        fun getInstance(): Conveyor {
-            return instance ?: throw IllegalArgumentException(
-                "Conveyor has not been initialized. Call initialize(io: ConveyorIO) first."
+        fun getInstance():Conveyor{
+            return instance?:throw IllegalStateException(
+                "Conveyor has not been initialize. call initialize(io:ConveyorIO) first"
             )
         }
     }
-
-    init {
-        timer.start()
-        timer.reset()
-    }
-
-    fun setVelocity(velocity: MutableMeasure<Velocity<Angle>>): Command = run {
-        velocitySetpoint.mut_replace(velocity)
-        io.setVelocity(velocity)
-    }
-
-    fun feed() = setVelocity(ConveyorConstants.FEED_VELOCITY)
-
-    @AutoLogOutput
-    fun atSetPoint(): Boolean {
-        return inputs.velocity.isNear(velocitySetpoint, ConveyorConstants.AT_SETPOINT_TOLERANCE.`in`(Units.Percent))
-    }
-
-    fun stop(): Command {
-        return runOnce {
-            velocitySetpoint.mut_replace(0.0, Units.RotationsPerSecond)
-            io.stop()
-        }
-    }
+    fun setPower(power:Double):Command=Commands.run({io.setSpinPower(power)})
+    fun conveyorIn():Command = Commands.run({io.setSpinPower(ConveyorConstants.CONVEYOR_POWER)})
+    fun conveyorOut():Command = Commands.run({io.setSpinPower(-ConveyorConstants.CONVEYOR_POWER)})
+    fun stopGripper():Command = Commands.run({io.setSpinPower(0.0)})
 
     override fun periodic() {
-        io.updateInputs()
-        if (timer.advanceIfElapsed(0.1)) Logger.processInputs(this::class.simpleName, inputs)
+        io.updateInput()
+        Logger.processInputs("Conveyor",inputs)
     }
 }
