83958a92d8bc699c985905b85556fc2128f25b38
yahali david mor <mor.yahali1612@gmail.com>
2024-09-11 18:44:09 +0300

    update Hood.kt

--- src/main/kotlin/frc/robot/subsystems/hood/Hood.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/Hood.kt
@@ -1,12 +1,20 @@
 package frc.robot.subsystems.hood
 
+import edu.wpi.first.units.Angle
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.MutableMeasure
+import edu.wpi.first.units.Units
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import frc.robot.Constants
+import org.littletonrobotics.junction.AutoLogOutput
+import org.littletonrobotics.junction.Logger
 
 class Hood private constructor(private var io: HoodIO):SubsystemBase(){
     private val inputs = io.inputs
+
+        @AutoLogOutput
+        var angleSetPoint:MutableMeasure<Angle> = MutableMeasure.zero(Units.Rotations)
 
     companion object{
         @Volatile
@@ -25,10 +33,18 @@ class Hood private constructor(private var io: HoodIO):SubsystemBase(){
             )
         }
     }
-    fun setAngle(angle:Double):Command = Commands.run({io.setAngle(angle)}).withName("set Angle Hood")
+    fun setAngle(angle:Measure<Angle>):Command = Commands.run({
+        io.setAngle(angle)
+        angleSetPoint = angle.mutableCopy()
+    }).withName("set Angle Hood")
 
     fun setRestAngle():Command = Commands.run({io.setAngle(HoodConstants.restAngle)})
 
-    fun atSetPoint():Boolean = inputs.angle.isNear(inputs.angleSetPoint,HoodConstants.TOLERANCE)
+    fun atSetPoint():Boolean = inputs.angle.isNear(angleSetPoint,HoodConstants.TOLERANCE)
 
+
+    override fun periodic() {
+        io.updateInputs()
+        Logger.processInputs("Hood",inputs)
+    }
 }
