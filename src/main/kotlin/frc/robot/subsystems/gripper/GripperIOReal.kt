38b919e40e9b049c346003306bb1a279178a18cd
yahali david mor <mor.yahali1612@gmail.com>
2024-09-11 18:44:54 +0300

    update GripperIOReal.kt

--- src/main/kotlin/frc/robot/subsystems/gripper/GripperIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperIOReal.kt
@@ -1,40 +1,32 @@
 package frc.robot.subsystems.gripper
 
-import com.revrobotics.CANSparkBase
 import com.revrobotics.CANSparkLowLevel
 import com.revrobotics.CANSparkMax
-import com.revrobotics.SparkLimitSwitch
-import edu.wpi.first.units.Units
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.MutableMeasure
+import edu.wpi.first.units.Power
+import edu.wpi.first.units.Voltage
 import edu.wpi.first.wpilibj.DigitalInput
-import edu.wpi.first.wpilibj.Timer
 import frc.robot.Ports
 
-class GripperIOReal : GripperIO {
-    override val inputs = LoggedGripperInputs()
-    private val rollerMotor: CANSparkMax =
+class GripperIOReal:GripperIO {
+    private val spinMotor: CANSparkMax =
         CANSparkMax(Ports.Gripper.ROLLER_ID, CANSparkLowLevel.MotorType.kBrushless)
-    private val timer = Timer()
-    private val sensor: DigitalInput = DigitalInput(1)
+    private val sensor: DigitalInput = DigitalInput(8)
+    override val inputs: LoggedGripperInputs = LoggedGripperInputs()
 
-    init {
-        rollerMotor.restoreFactoryDefaults()
-        rollerMotor.setSmartCurrentLimit(GripperConstants.currentLimit.`in`(Units.Amp).toInt())
-        rollerMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen).enableLimitSwitch(false)
-        rollerMotor.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen).enableLimitSwitch(false)
-        rollerMotor.setIdleMode(CANSparkBase.IdleMode.kBrake)
-        rollerMotor.inverted = GripperConstants.ROLLER_INVERTED_VALUE
-        rollerMotor.burnFlash()
-
-        timer.start()
-        timer.reset()
+    override fun setPower(power:Double) {
+        spinMotor.set(power)
     }
 
-    override fun setRollerMotorPower(power: Double) {
-        rollerMotor.set(power)
+    override fun hasNote() {
+        sensor.get()
     }
+
 
     override fun updateInputs() {
-        inputs.rollerMotorVoltage.mut_replace(rollerMotor.busVoltage, Units.Volts)
+        inputs.spinMotorPower = spinMotor.get()
         inputs.hasNote = !sensor.get()
     }
+
 }
