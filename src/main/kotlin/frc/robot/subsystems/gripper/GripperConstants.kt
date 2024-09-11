61287949b9a8159dd3df9f5b3a2d4be24ad20c7f
yahali david mor <mor.yahali1612@gmail.com>
2024-09-11 18:45:05 +0300

    update GripperConstants.kt

--- src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
@@ -1,11 +1,11 @@
 package frc.robot.subsystems.gripper
 
-import com.ctre.phoenix6.signals.InvertedValue
 import edu.wpi.first.units.Current
 import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 
 object GripperConstants {
-    const val ROLLER_INVERTED_VALUE = true
-    val currentLimit : Measure<Current> = Units.Amp.of(40.0)
+    const val GRIPPER_POWER = 0.7
+    const val IS_ROLL_INVERTED_VALUE = false
+    val currentLimiter:Measure<Current> = Units.Amps.of(40.0)
 }
