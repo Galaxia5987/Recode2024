5ef2146d972c39d1ba692c0d72db285f4283184b
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-09-11 11:39:11 +0300

    Calibrate Hood

--- src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
@@ -29,12 +29,12 @@ object HoodConstants {
     val INVERTED_VALUE = InvertedValue.CounterClockwise_Positive
 
 
-    val ABSOLUTE_ENCODER_OFFSET = LoggedTunableNumber("Hood/EncoderOffset", (58.095 - 33.48) / 360.0)
+    val ABSOLUTE_ENCODER_OFFSET = LoggedTunableNumber("Hood/EncoderOffset", (160.8309 - 33.48) / 360.0)
 
     val GAINS by lazy {
         selectGainsBasedOnMode(
             Gains(
-                2400.0, 0.0, 600.0, 0.0, 0.0, 0.0, 9.0
+                kP = 1700.0, kD = 305.0, kG =  5.5
             ), Gains(
                 20.0
             )
