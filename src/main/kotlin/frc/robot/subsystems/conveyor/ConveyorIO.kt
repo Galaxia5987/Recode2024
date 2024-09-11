7a0df6cd4871a5a7fe2936d458607614ddf9f7f5
yahali david mor <mor.yahali1612@gmail.com>
2024-09-11 18:50:57 +0300

    update ConveyorIO.kt

--- src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorIO.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorIO.kt
@@ -1,33 +1,16 @@
 package frc.robot.subsystems.conveyor
 
-import edu.wpi.first.units.Angle
-import edu.wpi.first.units.MutableMeasure
-import edu.wpi.first.units.Units
-import edu.wpi.first.units.Velocity
-import frc.robot.lib.webconstants.LoggedTunableNumber
 import org.team9432.annotation.Logged
 
 interface ConveyorIO {
-    val inputs: LoggedConveyorInputs
+    var inputs:LoggedConveyorInputs
 
-    fun setVelocity(velocity: MutableMeasure<Velocity<Angle>>) {}
+    fun updateInput()
 
-    fun updateInputs() {}
-
-    fun stop() {}
-
-    fun updatePID() {}
-
-    fun hasPIDChanged(PIDValues: Array<LoggedTunableNumber>): Boolean {
-        var hasChanged = false
-        for (value in PIDValues) {
-            if (value.hasChanged()) hasChanged = true
-        }
-        return hasChanged
-    }
+    fun setSpinPower(power:Double)
 
     @Logged
-    open class ConveyorInputs {
-        var velocity: MutableMeasure<Velocity<Angle>> = MutableMeasure.zero(Units.RotationsPerSecond)
+    open class ConveyorInputs{
+        var spinMotorPower:Double = 0.0
     }
 }
