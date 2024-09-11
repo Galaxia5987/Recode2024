355c9e21bf79948eba133819506c1b3adc5cb859
yahali david mor <mor.yahali1612@gmail.com>
2024-09-11 18:44:00 +0300

    update HoodConstants.kt

--- src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
@@ -6,13 +6,17 @@ import com.ctre.phoenix6.configs.Slot0Configs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
+import edu.wpi.first.units.Angle
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.MutableMeasure
+import edu.wpi.first.units.Units
 import frc.robot.Constants
 import frc.robot.subsystems.intake.IntakeConstants
 import frc.robot.subsystems.intake.IntakeConstants.GEAR_RATIO
 
 object HoodConstants {
     var MOTOR_CONFIGURATION = TalonFXConfiguration()
-    var restAngle = 35.0
+    var restAngle:Measure<Angle> = MutableMeasure.ofBaseUnits(100.0,Units.Degree)
     const val GEAR_RATIO: Double = 3.0 * (36.0 / 18.0) * (158.0 / 18.0)
     var ANGLE_KP = 0.0
     var ANGLE_KD = 0.0
