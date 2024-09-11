c2bc430dfc0319b24e1d012f20522e8bde63c2b9
yahali david mor <mor.yahali1612@gmail.com>
2024-09-11 18:43:46 +0300

    update HoodIOReal.kt

--- src/main/kotlin/frc/robot/subsystems/hood/HoodIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodIOReal.kt
@@ -3,7 +3,7 @@ package frc.robot.subsystems.hood
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import com.ctre.phoenix6.hardware.TalonFX
 import edu.wpi.first.units.Angle
-import edu.wpi.first.units.MutableMeasure
+import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 import frc.robot.Ports
 
@@ -13,14 +13,13 @@ class HoodIOReal() :HoodIO {
     private var encoder:TalonSRX = TalonSRX(Ports.Hood.ENCODER_ID)
 
 
-    override fun updateInput() {
+    override fun setAngle(angle: Measure<Angle>) {
+        angleMotor.setPosition(angle.`in`(Units.Rotation))
+    }
+
+    override fun updateInputs() {
         inputs.angle.mut_replace(angleMotor.position.value,Units.Rotations)
         inputs.angleMotorVoltage.mut_replace(angleMotor.supplyVoltage.value,Units.Volts)
         inputs.encoderPosition.mut_replace(encoder.selectedSensorPosition,Units.Rotations)
-    }
-
-    override fun setAngle(angle: Double) {
-        angleMotor.setPosition(angle)
-        inputs.angleSetPoint.mut_replace(angle,Units.Rotations)
     }
 }
