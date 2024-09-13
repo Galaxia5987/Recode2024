6a6b16e117144315bb90d30eba999aa261f23513
Emma Livne <emmalivne.03@gmail.com>
2024-09-13 13:28:52 +0300

    add constants

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
@@ -1,8 +1,17 @@
 package frc.robot.subsystems.intake
 
+import edu.wpi.first.units.Angle
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.Units
+
 object IntakeConstants {
     const val gearRatio:Double=1.0
     val kp=0.0
     val ki=0.0
     val kd=0.0
+    val INTAKE_ANGLE:Measure<Angle> = Units.Degree.of(21.5)
+    val REST_ANGLE:Measure<Angle> = Units.Degree.of(127.0)
+    const val INTAKE_ROLLER_POWER = -0.4
+    const val INTAKE_CENTER_POWER = -0.4
+
 }
