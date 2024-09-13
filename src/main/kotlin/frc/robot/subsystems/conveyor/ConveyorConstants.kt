74f7be074884adb49dedb5ad5317e1b843921feb
yahali david mor <mor.yahali1612@gmail.com>
2024-09-13 11:12:14 +0300

    update ConveyorConstants.kt

--- src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
@@ -6,7 +6,6 @@ import edu.wpi.first.units.Units
 
 object ConveyorConstants {
     const val GEAR_RATIO = 0.0
-    const val CONVEYOR_POWER = 0.7
     const val IS_ROLL_INVERTED_VALUE = false
     val currentLimiter: Measure<Current> = Units.Amps.of(40.0)
 }
