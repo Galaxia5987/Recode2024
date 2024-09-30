fd6820002b515f822d5fd308b13a303518f7ede2
Emma Livne <emmalivne.03@gmail.com>
2024-09-30 16:55:11 +0300

    Add timeout to amp shooting

--- src/main/kotlin/frc/robot/scoreState/AmpState.kt
+++ src/main/kotlin/frc/robot/scoreState/AmpState.kt
@@ -3,7 +3,6 @@ package frc.robot.scoreState
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import frc.robot.ControllerInputs
-import frc.robot.lib.handleInterrupt
 import frc.robot.subsystems.conveyor.Conveyor
 import frc.robot.subsystems.gripper.Gripper
 import frc.robot.subsystems.hood.Hood
@@ -43,6 +42,6 @@ class AmpState : ScoreState {
     }
 
     override fun execute(): Command {
-        return init().handleInterrupt(end())
+        return init().withTimeout(0.2).andThen(end())
     }
 }
