2f8542f14bd63eeb1e80191617392eb8b111318b
yahali david mor <mor.yahali1612@gmail.com>
2024-10-08 18:40:13 +0300

    Add tolerance constant

--- src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
@@ -5,5 +5,6 @@ import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 
 object ConveyorConstants {
+  const val TOLERANCE = 0.5
   private val CURRENT_LIMIT: Measure<Current> = Units.Amps.of(40.0)
 }
