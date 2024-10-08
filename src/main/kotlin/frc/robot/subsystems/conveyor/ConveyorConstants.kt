155f0546ac8acaacde7d24b4ba30bb1c87113573
yahali david mor <mor.yahali1612@gmail.com>
2024-10-08 18:37:19 +0300

    Remove redundant constants

--- src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
@@ -5,7 +5,5 @@ import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 
 object ConveyorConstants {
-    const val GEAR_RATIO = 0.0
-    const val IS_ROLL_INVERTED_VALUE = false
     val currentLimiter: Measure<Current> = Units.Amps.of(40.0)
 }
