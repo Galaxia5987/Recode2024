834c1346f2c1e5be4c56df642a7bb61f29917461
yahali david mor <mor.yahali1612@gmail.com>
2024-10-08 18:42:49 +0300

    Add configuration object

--- src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.conveyor
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs
+import com.ctre.phoenix6.configs.MotorOutputConfigs
+import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.signals.InvertedValue
 import edu.wpi.first.units.Current
 import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
@@ -9,4 +13,16 @@ object ConveyorConstants {
   private val CURRENT_LIMIT: Measure<Current> = Units.Amps.of(40.0)
   const val RUN_POWER: Double = 0.7
 
+    val CONFIG = TalonFXConfiguration().apply {
+        MotorOutput = MotorOutputConfigs().apply {
+            InvertedValue.Clockwise_Positive
+        }
+        CurrentLimits = CurrentLimitsConfigs().apply {
+            val currentLimit = CURRENT_LIMIT.`in`(Units.Amps)
+            SupplyCurrentLimit = currentLimit
+            StatorCurrentLimit = currentLimit * 2
+            StatorCurrentLimitEnable = true
+            SupplyCurrentLimitEnable = true
+        }
+    }
 }
