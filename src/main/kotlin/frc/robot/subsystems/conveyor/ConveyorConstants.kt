22efa09ca0d1a02525876b9f3c71028d5a088b97
yahali david mor <mor.yahali1612@gmail.com>
2024-10-08 18:39:48 +0300

    Make constant private

--- src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
@@ -5,5 +5,5 @@ import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 
 object ConveyorConstants {
-   val CURRENT_LIMIT: Measure<Current> = Units.Amps.of(40.0)
+  private val CURRENT_LIMIT: Measure<Current> = Units.Amps.of(40.0)
 }
