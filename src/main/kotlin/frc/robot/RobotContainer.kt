6a971819a25cedde74f7a5005d8f494fef092ac0
Emma Livne <emmalivne.03@gmail.com>
2024-10-10 20:05:32 +0300

    Register auto commands

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
 import frc.robot.commandGroups.IntakeCommands
 import frc.robot.commandGroups.ShootingCommands
+import frc.robot.commandGroups.WarmupCommands
 import frc.robot.scoreState.AmpState
 import frc.robot.scoreState.ClimbState
 import frc.robot.scoreState.ScoreState
@@ -15,6 +16,7 @@ import frc.robot.scoreState.ShootState
 import frc.robot.subsystems.climb.Climb
 import frc.robot.subsystems.gripper.Gripper
 import frc.robot.subsystems.intake.Intake
+import frc.robot.subsystems.shooter.Shooter
 import frc.robot.subsystems.swerve.SwerveDrive
 import frc.robot.ControllerInputs.driverController as driverController
 import frc.robot.ControllerInputs.operatorController as operatorController
@@ -77,5 +79,13 @@ object RobotContainer {
 
     private fun registerAutoCommands() {
         fun register(name: String, command: Command) = NamedCommands.registerCommand(name, command)
+        register("score", shootState.execute())
+        register("closeShoot", ShootingCommands.closeShoot().withTimeout(4.0))
+        register("warmup", WarmupCommands.warmup())
+        register("ampScore", ampState.execute())
+        register("intake", IntakeCommands.intake())
+        register("outtake", IntakeCommands.outtake())
+        register("stopIntake", IntakeCommands.stopIntake())
+        register("rollShooter", Shooter.getInstance().rollNote())
     }
 }
