5503f34cabd357a29c03db0752c7673a1c02f626
Emma Livne <emmalivne.03@gmail.com>
2024-10-10 20:06:36 +0300

    Update auto chooser's data in smart dashboard

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -3,6 +3,7 @@ package frc.robot
 import com.pathplanner.lib.auto.AutoBuilder
 import com.pathplanner.lib.auto.NamedCommands
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
@@ -41,6 +42,8 @@ object RobotContainer {
 
     init {
         currentState = shootState
+
+        SmartDashboard.putData("autoChooser", autoChooser)
 
         registerAutoCommands()
         configureButtonBindings()
