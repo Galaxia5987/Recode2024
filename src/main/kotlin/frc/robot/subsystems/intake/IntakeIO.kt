00ecd2aa280e5fedce284d030ecf6079c2a8a4df
nogav <noga.veksler@gmail.com>
2024-10-13 12:55:30 +0300

    create IntakeInputs

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeIO.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeIO.kt
@@ -1,4 +1,8 @@
 package frc.robot.subsystems.intake
+import edu.wpi.first.units.Angle
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.Units
+import org.team9432.annotation.Logged
 
 interface IntakeIO {
     fun setSpinPower(power:Double) {}
@@ -6,4 +10,11 @@ interface IntakeIO {
     fun setAnglePower(power: Double) {}
     fun setAngle(angle:Double) {}
     fun reset() {}
+
+    @Logged
+    open class IntakeInputs {
+        var spinmMotorVoltage = 0.0
+        var centerMotorVoltage = 0.0
+        var angleMotorAngle: Measure<Angle> = Units.Degree.zero()
+    }
 }
