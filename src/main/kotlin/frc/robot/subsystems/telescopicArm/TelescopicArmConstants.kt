3057a1d9f8c39a75475674855f87dfd7f89ea9c2
yahali david mor <mor.yahali1612@gmail.com>
2024-10-13 15:31:23 +0300

    Added gear ratio , moment of inertia , conversion factor to TelescopicArmConstants

--- src/main/kotlin/frc/robot/subsystems/telescopicArm/TelescopicArmConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/telescopicArm/TelescopicArmConstants.kt
@@ -10,6 +10,9 @@ object TelescopicArmConstants {
     const val KD = 0.0
     const val KI = 0.0
     const val KV = 0.0
+    const val GEAR_RATIO = 3.0
+    const val MOMENT_OF_INERTIA = 3.0
+    const val CONVERSION_FACTOR = 2 * Math.PI
     val CURRENT_LIMIT: Measure<Current> = Units.Amps.of(40.0)
     val dramRadius: Measure<Distance> = Units.Centimeter.of(3.0)
 }
