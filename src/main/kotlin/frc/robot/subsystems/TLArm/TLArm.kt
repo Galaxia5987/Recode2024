24a8860a10807a314f02576fd59ce47b8875d986
yahali david mor <mor.yahali1612@gmail.com>
2024-10-13 11:30:26 +0300

    Add initialize to TLArm

--- src/main/kotlin/frc/robot/subsystems/TLArm/TLArm.kt
+++ src/main/kotlin/frc/robot/subsystems/TLArm/TLArm.kt
@@ -1,5 +1,23 @@
 package frc.robot.subsystems.TLArm
 
 class TLArm private constructor(private var io: TLArmIO) {
+    private var inputs = io.inputs
 
+    companion object {
+        @Volatile
+        private var instance: TLArm? = null
+
+        fun initialize(io: TLArmIO) {
+            synchronized(true) {
+                if (instance == null) {
+                    instance = TLArm(io)
+                }
+            }
+        }
+
+        fun getInstance(): TLArm = instance ?: throw IllegalStateException(
+            "telescopic arm has not been initialized. Call initialize(io:TLArmIO) first"
+        )
+
+    }
 }
