2dc0df99c09e73e03cd345660fb65deb40d15379
nogav <noga.veksler@gmail.com>
2024-10-13 13:57:45 +0300

    add intake functions

--- src/main/kotlin/frc/robot/subsystems/intake/Intake.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/Intake.kt
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.intake
 
+import edu.wpi.first.units.Angle
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.Units
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -7,6 +10,57 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase
 class Intake private constructor(val io:IntakeIO):SubsystemBase() {
     fun intake():Command{
         Commands.parallel(io.setSpinPower(), io.setCenterPower(), io.setAngle())
+    fun intake():Command {
+       return Commands.parallel(
+            Commands.runOnce({io.setSpinPower(IntakeConstants.SPIN_POWER)}),
+            Commands.runOnce({io.setCenterPower(IntakeConstants.Centar_POWER)}),
+            Commands.runOnce({io.setAngle(IntakeConstants.ANGLE_DOWN)})
+       )
+    }
+    fun outtake():Command {
+        return Commands.parallel(
+            Commands.runOnce({io.setSpinPower(-IntakeConstants.SPIN_POWER)}),
+            Commands.runOnce({io.setCenterPower(-IntakeConstants.Centar_POWER)}),
+            Commands.runOnce({io.setAngle(IntakeConstants.ANGLE_UP)})
+        )
+    }
+    fun reset():Command{
+        return Commands.runOnce(
+            {io.setAnglePower(-0.3)}).finallyDo(
+            Runnable{ io.reset()
+            io.setAnglePower(0.0)})
+    }
+    fun stop(): Command {
+        return Commands.runOnce({
+        io.setAngle(IntakeConstants.ANGLE_UP)
+        io.setCenterPower(0.0)
+        io.setCenterPower(0.0)})
         }
 
 }
+    fun SetAngle(angle: Measure<Angle>): Command{
+        return Commands.run({
+            angleSetpoint=angle
+            io.setAngle(angle)
+
+        })
+    }
+
+    fun SetCenterPower(power: Double): Command{
+        return Commands.runOnce({
+            io.setCenterPower(power)
+        })
+    }
+
+    fun setSpinPower(power: Double): Command{
+        return Commands.runOnce({
+            io.setSpinPower(power)
+        })
+    }
+
+  override fun periodic() {
+        io.updateInputs()
+        Logger.processInputs(this::class.simpleName, io.inputs)
+    }
+}
+
