824e11c40c46e3a2b84dda4f2d7f481b64ff3dc3
nogav <noga.veksler@gmail.com>
2024-10-13 13:46:08 +0300

    add Constants

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
@@ -1,9 +1,17 @@
 package frc.robot.subsystems.intake
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration
+import edu.wpi.first.units.Angle
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.Units
 
 object IntakeConstants {
-    val ANGLE_MOTOR_CONFIGURATION=TalonFXConfiguration()
-    val SPIN_POWER=0.6
-
+    val GEAR_RATIO=55.56
+    val kP=55.0
+    val kI=3.0
+    val kD=0.1
+    val SPIN_POWER=0.4
+    val Centar_POWER=0.4
+    val ANGLE_UP: Measure<Angle> = Units.Degree.of(126.2)
+    val ANGLE_DOWN: Measure<Angle> = Units.Degree.of(6.5)
 }
