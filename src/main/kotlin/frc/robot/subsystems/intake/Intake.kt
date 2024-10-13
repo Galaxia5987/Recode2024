5104251a242981d9d6242246264e017390f132d8
nogav <noga.veksler@gmail.com>
2024-10-13 14:03:21 +0300

    remove unnecessary command

--- src/main/kotlin/frc/robot/subsystems/intake/Intake.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/Intake.kt
@@ -8,8 +8,6 @@ import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 class Intake private constructor(val io:IntakeIO):SubsystemBase() {
-    fun intake():Command{
-        Commands.parallel(io.setSpinPower(), io.setCenterPower(), io.setAngle())
 
     private var angleSetpoint: Measure<Angle> = Units.Degree.zero()
 
