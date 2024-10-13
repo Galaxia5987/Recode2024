b68a7a4c2cc12225131d0d627b021cda12ab67d9
yahali david mor <mor.yahali1612@gmail.com>
2024-10-13 11:46:34 +0300

    Add Commands to TLArm

--- src/main/kotlin/frc/robot/subsystems/TLArm/TLArm.kt
+++ src/main/kotlin/frc/robot/subsystems/TLArm/TLArm.kt
@@ -1,5 +1,10 @@
 package frc.robot.subsystems.TLArm
 
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.Unit
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
+
 class TLArm private constructor(private var io: TLArmIO) {
     private var inputs = io.inputs
 
@@ -18,6 +23,6 @@ class TLArm private constructor(private var io: TLArmIO) {
         fun getInstance(): TLArm = instance ?: throw IllegalStateException(
             "telescopic arm has not been initialized. Call initialize(io:TLArmIO) first"
         )
-
     }
+    fun setPosition(setPoint:Double):Command = Commands.runOnce({io.setPosition(setPoint)})
 }
