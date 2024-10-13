e5a85b1b01c2185d2141a62d66806d7aeb30bdc0
yahali david mor <mor.yahali1612@gmail.com>
2024-10-13 10:59:28 +0300

    Remove unnecessary functions

--- src/main/kotlin/frc/robot/subsystems/TLArm/TLArmConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/TLArm/TLArmConstants.kt
@@ -2,6 +2,7 @@ package frc.robot.subsystems.TLArm
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs
 import com.ctre.phoenix6.configs.MotorOutputConfigs
+import com.ctre.phoenix6.configs.Slot0Configs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
@@ -10,20 +11,12 @@ import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 
 object TLArmConstants {
-    val MOTOR_CONFIGURATION = TalonFXConfiguration()
     var KP = 0.0
     var KD = 0.0
     var KI = 0.0
     var KV = 0.0
-
     var CURRENT_LIMIT: Measure<Current> = Units.Amps.of(40.0)
+    val MOTOR_CONFIGURATION = TalonFXConfiguration()
 
-    fun a() {
-        val i = 2
-    }
-
-    fun b() {
-        val i = 3
-    }
 
 }
