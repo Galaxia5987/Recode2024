14c25cec573e9fef568ec1150f9cdad86cf2cdbc
Emma Livne <emmalivne.03@gmail.com>
2024-10-16 12:20:04 +0300

    Make controller not rumble in auto

--- src/main/kotlin/frc/robot/commandGroups/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/commandGroups/IntakeCommands.kt
@@ -3,6 +3,7 @@ package frc.robot.commandGroups
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import frc.robot.ControllerInputs
+import frc.robot.Robot
 import frc.robot.lib.finallyDo
 import frc.robot.subsystems.intake.Intake
 import frc.robot.subsystems.gripper.Gripper
@@ -18,7 +19,7 @@ object IntakeCommands {
             intake.intake(), gripper.setRollerPower(0.4)
         )
             .until { gripper.hasNote }
-            .andThen(Commands.parallel(intake.stop(), gripper.setRollerPower(0.0), ControllerInputs.startRumble()))
+            .andThen(Commands.parallel(intake.stop(), gripper.setRollerPower(0.0), ControllerInputs.startRumble().onlyIf{Robot.isTeleop}))
             .withName("intake")
     }
 
