486f3d99dcc1f885073d37bac45080d6fe71e3e2
Emma Livne <emmalivne.03@gmail.com>
2024-10-19 13:13:05 +0300

    Turn LEDs white when note has been intaked

--- src/main/kotlin/frc/robot/commandGroups/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/commandGroups/IntakeCommands.kt
@@ -2,17 +2,29 @@ package frc.robot.commandGroups
 
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.ConditionalCommand
 import frc.robot.ControllerInputs
 import frc.robot.Robot
+import frc.robot.RobotContainer
 import frc.robot.subsystems.intake.Intake
 import frc.robot.subsystems.gripper.Gripper
+import frc.robot.subsystems.leds.LEDConstants
 import frc.robot.subsystems.leds.LEDs
 
 object IntakeCommands {
     private val intake = Intake.getInstance()
     private val gripper = Gripper.getInstance()
 
-    fun stopIntake(): Command = Commands.parallel(intake.stop(), gripper.stop(), ControllerInputs.stopRumble())
+    fun stopIntake(): Command = Commands.parallel(
+        intake.stop(), gripper.stop(),
+        ControllerInputs.stopRumble()
+            .andThen(Commands.waitSeconds(2.0))
+            .andThen(
+                ConditionalCommand(
+                    LEDs.getInstance().setSolidMode(LEDConstants.SHOOT_STATE_COLOR),
+                    LEDs.getInstance().setSolidMode(LEDConstants.AMP_STATE_COLOR),
+                    { RobotContainer.getState()=="Speaker"})
+            ))
 
     fun intake(): Command {
         return Commands.parallel(
@@ -24,7 +36,7 @@ object IntakeCommands {
                     intake.stop(),
                     gripper.setRollerPower(0.0),
                     ControllerInputs.startRumble().onlyIf{Robot.isTeleop},
-                    LEDs.getInstance().intakeCommand()
+                    LEDs.getInstance().setSolidMode(LEDConstants.HAS_NOTE_COLOR)
                 ))
             .withName("intake")
     }
