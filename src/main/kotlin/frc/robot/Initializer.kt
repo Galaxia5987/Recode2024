644aa0cedbebe45efa5390e93f2dbd64a0608e80
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-10-21 16:50:41 +0300

    Create IO's map in Initializer

--- src/main/kotlin/frc/robot/Initializer.kt
+++ src/main/kotlin/frc/robot/Initializer.kt
@@ -15,10 +15,63 @@ import frc.robot.subsystems.leds.LEDs
 import frc.robot.subsystems.shooter.Shooter
 import frc.robot.subsystems.shooter.ShooterIOReal
 import frc.robot.subsystems.swerve.*
-import frc.robot.subsystems.vision.*
+import frc.robot.subsystems.vision.PhotonVisionIOReal
+import frc.robot.Constants.Mode
+import frc.robot.subsystems.climb.ClimbIO
+import frc.robot.subsystems.climb.LoggedClimbInputs
+import frc.robot.subsystems.conveyor.*
+import frc.robot.subsystems.gripper.*
+import frc.robot.subsystems.hood.*
+import frc.robot.subsystems.intake.*
+import frc.robot.subsystems.shooter.*
+import frc.robot.subsystems.vision.Vision
+import frc.robot.subsystems.vision.VisionConstants
 import org.photonvision.PhotonCamera
 
-object Initializer {
+val MAP = when (Constants.CURRENT_MODE) {
+    Mode.REAL -> mapOf(
+        Climb to ClimbIOTalonFX(),
+        Conveyor to ConveyorIOReal(),
+        Gripper to GripperIOReal(),
+        Intake to IntakeIOReal(),
+        Hood to HoodIOReal(),
+        LEDs to LEDs.initialize(9, 97),
+        Shooter to ShooterIOReal()
+    )
+
+    Mode.SIM -> mapOf(
+        Climb to object : ClimbIO {
+            override val inputs = LoggedClimbInputs()
+        },
+        Conveyor to ConveyorIOSim(),
+        Gripper to GripperIOSim(),
+        Intake to IntakeIOSim(),
+        Hood to HoodIOSim(),
+        Shooter to ShooterIOSim()
+    )
+
+    Mode.REPLAY -> mapOf(
+        Climb to object : ClimbIO {
+            override val inputs = LoggedClimbInputs()
+        },
+        Conveyor to object : ConveyorIO {
+            override val inputs = LoggedConveyorInputs()
+        },
+        Gripper to object : GripperIO {
+            override val inputs = LoggedGripperInputs()
+        },
+        Intake to object : IntakeIO {
+            override val inputs = LoggedIntakeInputs()
+        },
+        Hood to object : HoodIO {
+            override val inputs = LoggedHoodInputs()
+        },
+        Shooter to object : ShooterIO {
+            override val topRollerInputs = LoggedRollerInputs()
+            override val bottomRollerInputs = LoggedRollerInputs()
+        }
+    )
+}
 
     fun initSwerve() {
         val moduleIOs: Array<ModuleIO> = when (Constants.CURRENT_MODE) {
@@ -68,22 +121,22 @@ object Initializer {
         val speakerRightCamera =
             PhotonVisionIOReal(
                 PhotonCamera("rightOV2311"),
-                SPEAKER_RIGHT_CAMERA_POSE
+                VisionConstants.SPEAKER_RIGHT_CAMERA_POSE
             )
         val speakerLeftCamera =
             PhotonVisionIOReal(
                 PhotonCamera("leftOV2311"),
-                SPEAKER_LEFT_CAMERA_POSE,
+                VisionConstants.SPEAKER_LEFT_CAMERA_POSE,
             )
         val intakeAprilTagCamera =
             PhotonVisionIOReal(
                 PhotonCamera("frontOV2311"),
-                INTAKE_APRILTAG_CAMERA_POSE,
+                VisionConstants.INTAKE_APRILTAG_CAMERA_POSE,
             )
         val driverCamera =
             PhotonVisionIOReal(
                 PhotonCamera("Driver_Camera"),
-                DRIVER_CAMERA_POSE,
+                VisionConstants.DRIVER_CAMERA_POSE,
             )
 
         Vision.initialize(listOf(speakerRightCamera, speakerLeftCamera, intakeAprilTagCamera))
