f148f1a1dae70c96707eec2486361bed75803c85
Dan Katzuv <31829093+katzuv@users.noreply.github.com>
2024-11-02 18:13:24 +0200

    Improve names of metadata fields

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -49,11 +49,11 @@ object Robot : LoggedRobot() {
         Initializer //initialize all subsystems and constants
 
         // Initialize logger
-        Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME)
-        Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE)
-        Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA)
-        Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE)
-        Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH)
+        Logger.recordMetadata("Project name", BuildConstants.MAVEN_NAME)
+        Logger.recordMetadata("Build date", BuildConstants.BUILD_DATE)
+        Logger.recordMetadata("Last commit hash", BuildConstants.GIT_SHA)
+        Logger.recordMetadata("Last commit timestamp", BuildConstants.GIT_DATE)
+        Logger.recordMetadata("Branch name", BuildConstants.GIT_BRANCH)
         when (BuildConstants.DIRTY) {
             0 -> Logger.recordMetadata("GitDirty", "All changes committed")
             1 -> Logger.recordMetadata("GitDirty", "Uncommitted changes")
