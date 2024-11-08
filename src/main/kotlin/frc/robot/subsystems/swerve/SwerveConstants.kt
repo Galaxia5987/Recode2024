e6ad013b553dabca5dfc95794e903ac1a7b6edef
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-11-08 12:49:44 +0200

    Add type specification in SwerveConstants

--- src/main/kotlin/frc/robot/subsystems/swerve/SwerveConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/swerve/SwerveConstants.kt
@@ -28,22 +28,22 @@ object SwerveConstants {
 
     const val NEO_CURRENT_LIMIT = 40.0
     const val NEO_550_CURRENT_LIMIT = 20.0
-    val TALON_FX_CURRENT_LIMIT_CONFIGS = CurrentLimitsConfigs()
+    private val TALON_FX_CURRENT_LIMIT_CONFIGS: CurrentLimitsConfigs = CurrentLimitsConfigs()
         .withSupplyCurrentLimit(30.0)
         .withStatorCurrentLimit(60.0)
         .withStatorCurrentLimitEnable(true)
         .withSupplyCurrentLimitEnable(true)
-    val VOLTAGE_CONFIGS = VoltageConfigs()
+    private val VOLTAGE_CONFIGS: VoltageConfigs = VoltageConfigs()
         .withPeakForwardVoltage(VOLT_COMP_SATURATION)
         .withPeakReverseVoltage(VOLT_COMP_SATURATION)
-    val MOTOR_OUTPUT_CONFIGS = MotorOutputConfigs()
+    private val MOTOR_OUTPUT_CONFIGS: MotorOutputConfigs = MotorOutputConfigs()
         .withDutyCycleNeutralDeadband(NEUTRAL_DEADBAND)
         .withInverted(InvertedValue.Clockwise_Positive)
-    val MOTION_MAGIC_CONFIGS = MotionMagicConfigs()
+    private val MOTION_MAGIC_CONFIGS: MotionMagicConfigs = MotionMagicConfigs()
         .withMotionMagicCruiseVelocity(3.0)
         .withMotionMagicAcceleration(12.0)
-    val DRIVE_SLOT_0_CONFIG: Slot0Configs
-    val ANGLE_SLOT_0_CONFIG: Slot0Configs
+    private val DRIVE_SLOT_0_CONFIG: Slot0Configs
+    private val ANGLE_SLOT_0_CONFIG: Slot0Configs
 
     val STEERING_MULTIPLIER =
         LoggedTunableNumber("Steering multiplier", 0.6)
