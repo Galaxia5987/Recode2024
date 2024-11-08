2917cd8bb0d531a8de79ff974b0a29b5ce919fd2
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-11-08 12:47:02 +0200

    Add type specification in LEDConstants

--- src/main/kotlin/frc/robot/subsystems/leds/LEDConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/leds/LEDConstants.kt
@@ -3,9 +3,9 @@ package frc.robot.subsystems.leds
 import edu.wpi.first.wpilibj.util.Color
 
 object LEDConstants {
-    val SHOOT_STATE_COLOR = Color.kRed
-    val AMP_STATE_COLOR = Color.kBlue
-    val CLIMB_STATE_COLOR = Color.kYellow
-    val READY_TO_SHOOT_COLOR = Color.kGreen
-    val HAS_NOTE_COLOR = Color.kWhiteSmoke
+    val SHOOT_STATE_COLOR: Color = Color.kRed
+    val AMP_STATE_COLOR: Color = Color.kBlue
+    val CLIMB_STATE_COLOR: Color = Color.kYellow
+    val READY_TO_SHOOT_COLOR: Color = Color.kGreen
+    val HAS_NOTE_COLOR: Color = Color.kWhiteSmoke
 }
