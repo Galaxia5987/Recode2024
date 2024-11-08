564eac7ea7bc68fb7a6fef3f621fece9a47c6561
Emma Livne <emmalivne.03@gmail.com>
2024-11-08 10:20:18 +0200

    Add missing import

--- src/main/kotlin/frc/robot/subsystems/swerve/ModuleIOTalonFX.kt
+++ src/main/kotlin/frc/robot/subsystems/swerve/ModuleIOTalonFX.kt
@@ -8,6 +8,7 @@ import com.ctre.phoenix6.controls.VelocityVoltage
 import com.ctre.phoenix6.controls.VoltageOut
 import com.ctre.phoenix6.hardware.CANcoder
 import com.ctre.phoenix6.hardware.TalonFX
+import com.ctre.phoenix6.signals.NeutralModeValue
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.kinematics.SwerveModulePosition
 import edu.wpi.first.math.kinematics.SwerveModuleState
