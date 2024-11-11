12271afbfc3ee0c3af620326d051e27a170cdf64
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-11-11 12:01:59 +0200

    Create markAbnormalEvent command

--- src/main/kotlin/frc/robot/lib/MarkerCommands.kt
+++ src/main/kotlin/frc/robot/lib/MarkerCommands.kt
@@ -4,4 +4,8 @@ import edu.wpi.first.wpilibj.DataLogManager
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 
+const val ABNORMAL_EVENT_NAME = "EVENT"
+
 fun markEvent(eventName: String): Command = Commands.runOnce({ DataLogManager.log(eventName) })
+
+fun markAbnormalEvent(): Command = markEvent(ABNORMAL_EVENT_NAME)
