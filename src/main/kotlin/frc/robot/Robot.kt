7f76f338a1b2853544cd07ceda807f98f4af4a09
Emma Livne <emmalivne.03@gmail.com>
2024-11-11 14:20:16 +0200

    Remove unnecessary import

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.Compressor
 import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj.PneumaticsModuleType
 import edu.wpi.first.wpilibj.PowerDistribution
-import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
 import edu.wpi.first.wpilibj2.command.Commands
 import frc.robot.lib.PoseEstimation
