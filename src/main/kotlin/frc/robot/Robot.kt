a0c784e1112a28888a0bc915684918dadcc56793
Emma Livne <emmalivne.03@gmail.com>
2024-11-11 14:19:52 +0200

    Initialize autonomousCommand in Robot

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType
 import edu.wpi.first.wpilibj.PowerDistribution
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
+import edu.wpi.first.wpilibj2.command.Commands
 import frc.robot.lib.PoseEstimation
 import frc.robot.subsystems.vision.VISION_MEASUREMENT_MULTIPLIER
 import frc.robot.subsystems.swerve.SwerveDrive
@@ -33,7 +34,7 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter
  */
 object Robot : LoggedRobot() {
     private val compressor = Compressor(PneumaticsModuleType.CTREPCM)
-    private lateinit var autonomousCommand: Command
+    private var autonomousCommand = Commands.none()
 
     /**
      * This function is run when the robot is first started up and should be used for any
