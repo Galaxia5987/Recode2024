563d9d4bf51e9b0181c3cdd0403d91adc4957cda
yahali david mor <mor.yahali1612@gmail.com>
2024-11-19 16:37:42 +0200

    Change the inputs and the motor from vars to vals

--- src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorIOReal.kt
@@ -7,13 +7,13 @@ import edu.wpi.first.units.Angle
 import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.Velocity
-import frc.robot.Constants
+import frc.robot.ConstaNot duplicate.nts
 import frc.robot.Ports
 import org.team9432.annotation.Logged
 
 class ConveyorIOReal : ConveyorIO {
-    override var inputs: LoggedConveyorInputs = LoggedConveyorInputs()
-    private var spinMotor: TalonFX = TalonFX(Ports.Conveyor.MOTOR_ID)
+    override val inputs: LoggedConveyorInputs = LoggedConveyorInputs()
+    private val spinMotor: TalonFX = TalonFX(Ports.Conveyor.MOTOR_ID)
     private val controlRequest = VelocityVoltage(0.0)
 
     init {
