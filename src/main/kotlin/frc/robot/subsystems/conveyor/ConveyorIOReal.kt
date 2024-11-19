fd0cd0084f48fcc07b9637449ad247de67cb2433
yahali david mor <mor.yahali1612@gmail.com>
2024-11-19 16:38:23 +0200

    Fix typo

--- src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorIOReal.kt
@@ -2,14 +2,11 @@ package frc.robot.subsystems.conveyor
 
 import com.ctre.phoenix6.controls.VelocityVoltage
 import com.ctre.phoenix6.hardware.TalonFX
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id
 import edu.wpi.first.units.Angle
 import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.Velocity
-import frc.robot.ConstaNot duplicate.nts
 import frc.robot.Ports
-import org.team9432.annotation.Logged
 
 class ConveyorIOReal : ConveyorIO {
     override val inputs: LoggedConveyorInputs = LoggedConveyorInputs()
