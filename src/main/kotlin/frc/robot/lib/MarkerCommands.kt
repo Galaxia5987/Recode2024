52825e6518f65ce0966ba8e1d84c2d3255317faf
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-11-19 15:54:08 +0200

    Add alert

--- src/main/kotlin/frc/robot/lib/MarkerCommands.kt
+++ src/main/kotlin/frc/robot/lib/MarkerCommands.kt
@@ -3,6 +3,7 @@ package frc.robot.lib
 import edu.wpi.first.wpilibj.DataLogManager
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.WaitCommand
 
 const val ABNORMAL_EVENT_NAME = "EVENT"
 
