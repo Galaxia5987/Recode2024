39a45ed0266a107b72fe0f598694b494cf1d9765
yahali david mor <mor.yahali1612@gmail.com>
2024-11-22 10:53:23 +0200

    Removed init to apply immediately

--- src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
@@ -9,7 +9,7 @@ import edu.wpi.first.units.MutableMeasure
 import edu.wpi.first.units.Units
 
 object HoodConstants {
-    val MOTOR_CONFIGURATION = TalonFXConfiguration()
+
     val restAngle: Measure<Angle> = MutableMeasure.ofBaseUnits(100.0, Units.Degree)
     const val GEAR_RATIO: Double = 3.0 * (36.0 / 18.0) * (158.0 / 18.0)
     val ANGLE_KP = 0.0
@@ -17,28 +17,26 @@ object HoodConstants {
     val ANGLE_KI = 0.0
     const val TOLERANCE: Double = 0.75 / 360
 
-    init {
-
-        MOTOR_CONFIGURATION.apply {
-            MotorOutput.apply {
-                NeutralMode = NeutralModeValue.Brake
-                Inverted = InvertedValue.Clockwise_Positive
-            }
-            Feedback.apply {
-                RotorToSensorRatio = 1.0
-                SensorToMechanismRatio = GEAR_RATIO
-            }
-            Slot0.apply {
-                kP = ANGLE_KP
-                kI = ANGLE_KI
-                kD = ANGLE_KD
-            }
-            CurrentLimits.apply {
-                StatorCurrentLimitEnable = true
-                StatorCurrentLimit = 80.0
-                SupplyCurrentLimitEnable = true
-                SupplyCurrentLimit = 40.0
-            }
+    val MOTOR_CONFIGURATION = TalonFXConfiguration().apply {
+        MotorOutput.apply {
+            NeutralMode = NeutralModeValue.Brake
+            Inverted = InvertedValue.Clockwise_Positive
+        }
+        Feedback.apply {
+            RotorToSensorRatio = 1.0
+            SensorToMechanismRatio = GEAR_RATIO
+        }
+        Slot0.apply {
+            kP = ANGLE_KP
+            kI = ANGLE_KI
+            kD = ANGLE_KD
+        }
+        CurrentLimits.apply {
+            StatorCurrentLimitEnable = true
+            StatorCurrentLimit = 80.0
+            SupplyCurrentLimitEnable = true
+            SupplyCurrentLimit = 40.0
         }
     }
+
 }
