3b47d2a46a2ade084ba82c3e9de23f76b232f914
yahali david mor <mor.yahali1612@gmail.com>
2024-11-22 11:01:24 +0200

    Change the init of the motor config to apply

--- src/main/kotlin/frc/robot/subsystems/shooter/ShooterConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/ShooterConstants.kt
@@ -1,51 +1,53 @@
 package frc.robot.subsystems.shooter
 
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs
-import com.ctre.phoenix6.configs.MotorOutputConfigs
-import com.ctre.phoenix6.configs.Slot0Configs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.signals.InvertedValue
-import edu.wpi.first.units.Measure
 import edu.wpi.first.units.MutableMeasure
 import edu.wpi.first.units.Units
-import kotlin.math.PI
 
 object ShooterConstants {
-    var configTop: TalonFXConfiguration = TalonFXConfiguration()
-    var configBottom: TalonFXConfiguration = TalonFXConfiguration()
     var currentLimit: Double = 40.0
     var shootVelocity = MutableMeasure.ofBaseUnits(3.0, Units.RotationsPerSecond)
     var KP: Double = 0.0
     var KD: Double = 0.0
     var KI: Double = 0.0
     var KV: Double = 0.0
+    var CONNFIG_TOP: TalonFXConfiguration = TalonFXConfiguration().apply {
+        Slot0.apply {
+            kP = KP
+            kD = KD
+            kI = KI
+            kV = KV
 
-    init {
-        configTop.withCurrentLimits(
-            CurrentLimitsConfigs()
-                .withStatorCurrentLimit(currentLimit * 2)
-                .withSupplyCurrentLimit(currentLimit)
-                .withStatorCurrentLimitEnable(true)
-                .withSupplyCurrentLimitEnable(true)
-        ).withSlot0(
-            Slot0Configs()
-                .withKP(KP)
-                .withKD(KD)
-                .withKI(KI)
-                .withKV(KV)
-        ).withMotorOutput(MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive))
-        configBottom.withCurrentLimits(
-            CurrentLimitsConfigs()
-                .withStatorCurrentLimit(currentLimit * 2)
-                .withSupplyCurrentLimit(currentLimit)
-                .withStatorCurrentLimitEnable(true)
-                .withSupplyCurrentLimitEnable(true)
-        ).withSlot0(
-            Slot0Configs()
-                .withKP(KP)
-                .withKD(KD)
-                .withKI(KI)
-                .withKV(KV)
-        ).withMotorOutput(MotorOutputConfigs().withInverted(InvertedValue.CounterClockwise_Positive))
+        }
+        CurrentLimits.apply {
+            StatorCurrentLimitEnable = true
+            SupplyCurrentLimit = currentLimit
+            SupplyCurrentLimitEnable = true
+            StatorCurrentLimit = currentLimit * 2
+        }
+        MotorOutput.apply {
+            Inverted = InvertedValue.Clockwise_Positive
+        }
+    }
+
+
+    var CONNFIG_BOTTOM: TalonFXConfiguration = TalonFXConfiguration().apply {
+        Slot0.apply {
+            kP = KP
+            kD = KD
+            kI = KI
+            kV = KV
+
+        }
+        CurrentLimits.apply {
+            StatorCurrentLimitEnable = true
+            SupplyCurrentLimit = currentLimit
+            SupplyCurrentLimitEnable = true
+            StatorCurrentLimit = currentLimit * 2
+        }
+        MotorOutput.apply {
+            Inverted = InvertedValue.Clockwise_Positive
+        }
     }
 }
