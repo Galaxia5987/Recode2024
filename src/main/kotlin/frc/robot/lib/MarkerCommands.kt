85ff47319e8557057c71b668263931af1cb6f0a8
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-11-24 08:39:27 +0200

    Add alert to markEvent command

--- src/main/kotlin/frc/robot/lib/MarkerCommands.kt
+++ src/main/kotlin/frc/robot/lib/MarkerCommands.kt
@@ -1,12 +1,17 @@
 package frc.robot.lib
 
+import edu.wpi.first.wpilibj.Alert
 import edu.wpi.first.wpilibj.DataLogManager
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.WaitCommand
 
 const val ABNORMAL_EVENT_NAME = "EVENT"
+val alert = Alert(ABNORMAL_EVENT_NAME, Alert.AlertType.kWarning)
 
-fun markEvent(eventName: String): Command = Commands.runOnce({ DataLogManager.log(eventName) })
+fun markEvent(eventName: String): Command = Commands.runOnce({
+    DataLogManager.log(eventName)
+    alert.set(true)
+})
 
 fun markAbnormalEvent(): Command = markEvent(ABNORMAL_EVENT_NAME)
