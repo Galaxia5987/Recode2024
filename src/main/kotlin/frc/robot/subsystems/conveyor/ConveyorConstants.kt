bfeca50656422f46a78f57970eaaa6a8cf43fa68
yahali david mor <mor.yahali1612@gmail.com>
2024-11-29 13:53:02 +0200

    Add constants for sim

--- src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/conveyor/ConveyorConstants.kt
@@ -8,10 +8,13 @@ import edu.wpi.first.units.CurrentUnit
 import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Dimensionless
+import edu.wpi.first.units.measure.MomentOfInertia
 
 object ConveyorConstants {
+    const val GEAR_RATIO = 1.0
     val TOLERANCE: Dimensionless = Units.Percent.of(0.1)
     private val CURRENT_LIMIT: Measure<CurrentUnit> = Units.Amps.of(40.0)
+    val MOMENT_OF_INERTIA: MomentOfInertia = Units.KilogramSquareMeters.of(0.000_05)
     const val RUN_POWER: Double = 0.7
 
     val CONFIG = TalonFXConfiguration().apply {
