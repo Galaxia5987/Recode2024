7eb27872fd802303f0556432bce4ece452f11390
Emma Livne <emmalivne.03@gmail.com>
2024-12-01 13:30:35 +0200

    Comment vision stuff (Sorry Katsuv😿)

--- src/main/kotlin/frc/robot/subsystems/vision/PhotonVisionIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/vision/PhotonVisionIOReal.kt
@@ -1,51 +1,51 @@
 package frc.robot.subsystems.vision
-
-import edu.wpi.first.math.geometry.Transform3d
-import org.photonvision.PhotonCamera
-import org.photonvision.PhotonPoseEstimator
-
-class PhotonVisionIOReal(private val camera: PhotonCamera, private val robotToCam: Transform3d) : VisionIO {
-    override val inputs = LoggedVisionInputs()
-    private val estimator: PhotonPoseEstimator = PhotonPoseEstimator(
-        aprilTagFieldLayout,
-        PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-        robotToCam
-    )
-
-    override val name: String = camera.name
-
-    init {
-        camera.pipelineIndex = 0
-    }
-
-    override fun setPipeLine(pipeLineIndex: Int) {
-        camera.pipelineIndex = pipeLineIndex
-    }
-
-    override fun updateInputs() {
-        val unreadResults = camera.allUnreadResults
-
-        for (result in unreadResults) {
-            if (!result.hasTargets()) {
-                return
-            }
-
-            val estimatedPose = estimator.update(result)
-
-            if (estimatedPose.isEmpty) {
-                continue
-            }
-
-            val tags = result.targets
-
-            inputs.bestCameraToTargets.clear()
-            inputs.poseFieldOriented = estimatedPose.get().estimatedPose
-
-            inputs.timestamp = estimatedPose.get().timestampSeconds
-
-            for (tag in tags) {
-                inputs.bestCameraToTargets.add(tag.bestCameraToTarget)
-            }
-        }
-    }
-}
+//
+// import edu.wpi.first.math.geometry.Transform3d
+// import org.photonvision.PhotonCamera
+// import org.photonvision.PhotonPoseEstimator
+//
+// class PhotonVisionIOReal(private val camera: PhotonCamera, private val robotToCam: Transform3d) : VisionIO {
+//    override val inputs = LoggedVisionInputs()
+//    private val estimator: PhotonPoseEstimator = PhotonPoseEstimator(
+//        aprilTagFieldLayout,
+//        PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+//        robotToCam
+//    )
+//
+//    override val name: String = camera.name
+//
+//    init {
+//        camera.pipelineIndex = 0
+//    }
+//
+//    override fun setPipeLine(pipeLineIndex: Int) {
+//        camera.pipelineIndex = pipeLineIndex
+//    }
+//
+//    override fun updateInputs() {
+//        val unreadResults = camera.allUnreadResults
+//
+//        for (result in unreadResults) {
+//            if (!result.hasTargets()) {
+//                return
+//            }
+//
+//            val estimatedPose = estimator.update(result)
+//
+//            if (estimatedPose.isEmpty) {
+//                continue
+//            }
+//
+//            val tags = result.targets
+//
+//            inputs.bestCameraToTargets.clear()
+//            inputs.poseFieldOriented = estimatedPose.get().estimatedPose
+//
+//            inputs.timestamp = estimatedPose.get().timestampSeconds
+//
+//            for (tag in tags) {
+//                inputs.bestCameraToTargets.add(tag.bestCameraToTarget)
+//            }
+//        }
+//    }
+// }
--- src/main/kotlin/frc/robot/subsystems/vision/PhotonVisionIOSim.kt
+++ src/main/kotlin/frc/robot/subsystems/vision/PhotonVisionIOSim.kt
@@ -1,59 +1,59 @@
 package frc.robot.subsystems.vision
-
-import edu.wpi.first.math.geometry.Transform3d
-import frc.robot.lib.toPose3d
-import frc.robot.subsystems.swerve.SwerveDrive
-import org.photonvision.PhotonPoseEstimator
-import org.photonvision.estimation.TargetModel
-import org.photonvision.simulation.PhotonCameraSim
-import org.photonvision.simulation.VisionTargetSim
-
-class PhotonVisionIOSim(private val simCamera: PhotonCameraSim, private val robotToCam: Transform3d) : VisionIO {
-    override val inputs = LoggedVisionInputs()
-    private val estimator: PhotonPoseEstimator = PhotonPoseEstimator(
-        aprilTagFieldLayout,
-        PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-        robotToCam
-    )
-
-    override val name = simCamera.camera.name
-
-    init {
-        VisionSim.system.addAprilTags(aprilTagFieldLayout)
-        VisionSim.system.addCamera(simCamera, robotToCam)
-    }
-
-    override fun updateInputs() {
-        val botPose = SwerveDrive.getInstance().estimator.estimatedPosition
-        val botPose3d = botPose.toPose3d()
-        val aprilTags = aprilTagFieldLayout.tags.map {
-            VisionTargetSim(
-                it.pose,
-                TargetModel.kAprilTag36h11,
-                it.ID
-            )
-        }
-        val latestResult =
-            simCamera.process(
-                0.0,
-                botPose3d + robotToCam.inverse(),
-                aprilTags
-            )
-        simCamera.submitProcessedFrame(latestResult)
-
-        if (!latestResult.hasTargets()) {
-            return
-        }
-
-        val estimatedPose = estimator.update(latestResult)
-
-        inputs.poseFieldOriented = estimatedPose.get().estimatedPose
-        inputs.timestamp = estimatedPose.get().timestampSeconds
-
-        val tags = latestResult.targets
-
-        for (tag in tags) {
-            inputs.bestCameraToTargets.add(tag.bestCameraToTarget)
-        }
-    }
-}
+//
+// import edu.wpi.first.math.geometry.Transform3d
+// import frc.robot.lib.toPose3d
+// import frc.robot.subsystems.swerve.SwerveDrive
+// import org.photonvision.PhotonPoseEstimator
+// import org.photonvision.estimation.TargetModel
+// import org.photonvision.simulation.PhotonCameraSim
+// import org.photonvision.simulation.VisionTargetSim
+//
+// class PhotonVisionIOSim(private val simCamera: PhotonCameraSim, private val robotToCam: Transform3d) : VisionIO {
+//    override val inputs = LoggedVisionInputs()
+//    private val estimator: PhotonPoseEstimator = PhotonPoseEstimator(
+//        aprilTagFieldLayout,
+//        PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+//        robotToCam
+//    )
+//
+//    override val name = simCamera.camera.name
+//
+//    init {
+//        VisionSim.system.addAprilTags(aprilTagFieldLayout)
+//        VisionSim.system.addCamera(simCamera, robotToCam)
+//    }
+//
+//    override fun updateInputs() {
+//        val botPose = SwerveDrive.getInstance().estimator.estimatedPosition
+//        val botPose3d = botPose.toPose3d()
+//        val aprilTags = aprilTagFieldLayout.tags.map {
+//            VisionTargetSim(
+//                it.pose,
+//                TargetModel.kAprilTag36h11,
+//                it.ID
+//            )
+//        }
+//        val latestResult =
+//            simCamera.process(
+//                0.0,
+//                botPose3d + robotToCam.inverse(),
+//                aprilTags
+//            )
+//        simCamera.submitProcessedFrame(latestResult)
+//
+//        if (!latestResult.hasTargets()) {
+//            return
+//        }
+//
+//        val estimatedPose = estimator.update(latestResult)
+//
+//        inputs.poseFieldOriented = estimatedPose.get().estimatedPose
+//        inputs.timestamp = estimatedPose.get().timestampSeconds
+//
+//        val tags = latestResult.targets
+//
+//        for (tag in tags) {
+//            inputs.bestCameraToTargets.add(tag.bestCameraToTarget)
+//        }
+//    }
+// }
