12c01f00111f9e5b15a4263b63a5bcfc2b12c8f6
Emma Livne <emmalivne.03@gmail.com>
2024-12-05 23:13:04 +0200

    Remove unnecessary things in robot container

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -1,16 +1,10 @@
 package frc.robot
 
-import com.pathplanner.lib.auto.AutoBuilder
 import com.pathplanner.lib.auto.NamedCommands
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
-import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers
-import frc.robot.ControllerInputs.driverController
 import frc.robot.lib.enableAutoLogOutputFor
-import frc.robot.subsystems.swerve.SwerveDrive
-import org.littletonrobotics.junction.networktables.LoggedDashboardChooser
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,11 +13,7 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 object RobotContainer {
-    private val swerveDrive = SwerveDrive.getInstance()
-
     private val testController = CommandXboxController(2)
-
-    private val autoChooser: LoggedDashboardChooser<Command>
 
     init {
         enableAutoLogOutputFor(this)
@@ -31,29 +21,15 @@ object RobotContainer {
         registerAutoCommands()
         configureButtonBindings()
         configureDefaultCommands()
-
-        swerveDrive.configAutoBuilder()
-
-        autoChooser = LoggedDashboardChooser("AutoChooser", AutoBuilder.buildAutoChooser())
-        SmartDashboard.putData("autoChooser", autoChooser.sendableChooser)
     }
 
     private fun configureDefaultCommands() {
-        swerveDrive.defaultCommand = swerveDrive.driveCommand(
-            { -driverController().leftY },
-            { -driverController().leftX },
-            { 0.5 * -driverController().rightX }
-        )
     }
 
     private fun configureButtonBindings() {
-        RobotModeTriggers.autonomous().or(RobotModeTriggers.teleop()).onTrue(swerveDrive.setBrakeMode())
-        RobotModeTriggers.disabled().debounce(7.0).onTrue(swerveDrive.setCoastMode())
-
-        driverController().y().onTrue(Commands.runOnce(swerveDrive::resetGyro))
     }
 
-    fun getAutonomousCommand(): Command = autoChooser.get()
+    fun getAutonomousCommand(): Command = Commands.none()
 
     private fun registerAutoCommands() {
         fun register(name: String, command: Command) = NamedCommands.registerCommand(name, command)
