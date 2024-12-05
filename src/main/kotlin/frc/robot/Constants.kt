6eb4102f5eea2a8263ac5f01e9a610014da58a9e
Emma Livne <emmalivne.03@gmail.com>
2024-12-05 23:10:55 +0200

    Remove unnecessary constants

--- src/main/kotlin/frc/robot/Constants.kt
+++ src/main/kotlin/frc/robot/Constants.kt
@@ -1,60 +1,18 @@
 package frc.robot
 
-import com.pathplanner.lib.path.PathConstraints
-import edu.wpi.first.math.geometry.Pose2d
-import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.geometry.Translation2d
-import edu.wpi.first.units.Units
-import edu.wpi.first.units.measure.AngularAcceleration
-import edu.wpi.first.units.measure.AngularVelocity
-import edu.wpi.first.units.measure.Distance
-import edu.wpi.first.units.measure.LinearAcceleration
-import edu.wpi.first.units.measure.LinearVelocity
 import edu.wpi.first.wpilibj.DriverStation
-import frc.robot.lib.getPoseByColor
 import frc.robot.lib.getTranslationByColor
-import frc.robot.subsystems.swerve.SwerveConstants
 import org.littletonrobotics.junction.LoggedRobot
-import kotlin.math.sqrt
 
 object Constants {
-    const val CONFIG_TIMEOUT: Int = 100 // [ms]
     const val LOOP_TIME = 0.02 // [s]
     const val IS_TUNING_MODE = true
-
-    private val EFFECTIVE_ROBOT_RADIUS: Distance = Units.Meters.of(SwerveConstants.ROBOT_LENGTH / sqrt(2.0))
-    private val MAX_VELOCITY: LinearVelocity = Units.MetersPerSecond.of(4.5)
-    private val MAX_ACCELERATION: LinearAcceleration = Units.MetersPerSecondPerSecond.of(3.0)
-    private val MAX_ANGULAR_VELOCITY: AngularVelocity = Units.RotationsPerSecond.of(
-        MAX_VELOCITY.`in`(Units.MetersPerSecond) / EFFECTIVE_ROBOT_RADIUS.`in`(Units.Meters)
-    )
-    private val MAX_ANGULAR_ACCELERATION: AngularAcceleration =
-        Units.RotationsPerSecond.per(Units.Second).of(
-            MAX_ACCELERATION.`in`(Units.MetersPerSecondPerSecond) / EFFECTIVE_ROBOT_RADIUS.`in`(Units.Meters)
-        )
-    val PATH_CONSTRAINTS: PathConstraints = PathConstraints(
-        MAX_VELOCITY.`in`(Units.MetersPerSecond),
-        MAX_ACCELERATION.`in`(Units.MetersPerSecondPerSecond),
-        MAX_ANGULAR_VELOCITY.`in`(Units.RotationsPerSecond),
-        MAX_ANGULAR_ACCELERATION.`in`(Units.RotationsPerSecond.per(Units.Second))
-    )
 
     private val SPEAKER_POSE_BLUE = Translation2d(0.0, 5.5479442)
 
     val SPEAKER_POSE: Translation2d
         get() = getTranslationByColor(SPEAKER_POSE_BLUE)
-
-    val CHAIN_LOCATIONS: List<Pose2d> by lazy {
-        val blueChainLocations = listOf(
-            Triple(4.39, 4.67, -57.72), // Left
-            Triple(5.59, 4.09, 180.00), // Center
-            Triple(4.39, 3.46, 57.72) // Right
-        ).map { (x, y, theta) -> Pose2d(x, y, Rotation2d.fromDegrees(theta)) }
-
-        blueChainLocations.map { pose ->
-            getPoseByColor(pose)
-        }
-    }
 
     val CURRENT_MODE: Mode
         get() =
@@ -63,7 +21,9 @@ object Constants {
             } else {
                 if (System.getenv()["isReplay"] == "true") {
                     Mode.REPLAY
-                } else Mode.SIM
+                } else {
+                    Mode.SIM
+                }
             }
     const val ROBORIO_NEO_SERIAL = "030e2d4d"
 
@@ -72,9 +32,6 @@ object Constants {
 
     val IS_RED: Boolean
         get() = DriverStation.getAlliance().isPresent && DriverStation.getAlliance().get() == DriverStation.Alliance.Red
-
-    const val FIELD_LENGTH: Double = 16.54
-    const val FIELD_WIDTH: Double = 8.23
 
     enum class Mode {
         REAL, SIM, REPLAY
