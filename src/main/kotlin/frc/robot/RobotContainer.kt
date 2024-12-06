c5d2983ed7010e681dfa5dc7859d3bdb96fe2dc5
Emma Livne <emmalivne.03@gmail.com>
2024-12-06 02:10:03 +0200

    Initialize swerveDrive

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -4,6 +4,14 @@ import com.pathplanner.lib.auto.NamedCommands
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
+import frc.robot.generated.TunerConstants
+import frc.robot.subsystems.drive.Drive
+import frc.robot.subsystems.drive.DriveCommands
+import frc.robot.subsystems.drive.GyroIO
+import frc.robot.subsystems.drive.GyroIONavX
+import frc.robot.subsystems.drive.ModuleIO
+import frc.robot.subsystems.drive.ModuleIOSim
+import frc.robot.subsystems.drive.ModuleIOTalonFX
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -12,9 +20,39 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 object RobotContainer {
+    private val swerveDrive: Drive
     private val testController = CommandXboxController(2)
 
     init {
+        swerveDrive = when(Constants.CURRENT_MODE){
+            Constants.Mode.REAL -> {
+                Drive(
+                    GyroIONavX(),
+                    ModuleIOTalonFX(TunerConstants.FrontLeft),
+                    ModuleIOTalonFX(TunerConstants.FrontRight),
+                    ModuleIOTalonFX(TunerConstants.BackLeft),
+                    ModuleIOTalonFX(TunerConstants.BackRight)
+                )
+            }
+            Constants.Mode.SIM -> {
+                Drive(
+                    object: GyroIO {},
+                    ModuleIOSim(TunerConstants.FrontLeft),
+                    ModuleIOSim(TunerConstants.FrontRight),
+                    ModuleIOSim(TunerConstants.BackLeft),
+                    ModuleIOSim(TunerConstants.BackRight)
+                )
+            }
+            else -> {
+                Drive(
+                    object: GyroIO {},
+                    object: ModuleIO {},
+                    object: ModuleIO {},
+                    object: ModuleIO {},
+                    object: ModuleIO {}
+                )
+            }
+        }
         registerAutoCommands()
         configureButtonBindings()
         configureDefaultCommands()
