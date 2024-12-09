7ef09daf3f6f82a11369948a0895a60b1fd09078
Emma Livne <emmalivne.03@gmail.com>
2024-12-09 16:49:52 +0200

    Calibrate PID values (WIP)

--- src/main/kotlin/frc/robot/generated/TunerConstants.java
+++ src/main/kotlin/frc/robot/generated/TunerConstants.java
@@ -22,17 +22,22 @@ public class TunerConstants {
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains =
             new Slot0Configs()
-                    .withKP(100)
+                    .withKP(50)
                     .withKI(0)
-                    .withKD(2.0)
-                    .withKS(0.2)
-                    .withKV(1.59)
+                    .withKD(0)
+                    .withKS(0)
+                    .withKV(0)
                     .withKA(0)
                     .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains =
-            new Slot0Configs().withKP(0.1).withKI(0).withKD(0).withKS(0).withKV(0.124);
+            new Slot0Configs()
+                    .withKP(1)
+                    .withKI(0)
+                    .withKD(0)
+                    .withKS(0)
+                    .withKV(0);
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
