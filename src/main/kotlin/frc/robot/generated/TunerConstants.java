1624b2ba5b4ede236017592f541c185e04052236
Emma Livne <emmalivne.03@gmail.com>
2024-12-09 16:51:07 +0200

    Calibrate max speed (WIP)

--- src/main/kotlin/frc/robot/generated/TunerConstants.java
+++ src/main/kotlin/frc/robot/generated/TunerConstants.java
@@ -78,7 +78,7 @@ public class TunerConstants {
 
     // Theoretical free speed (m/s) at 12 V applied output;
     // This needs to be tuned to your individual robot
-    public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(4.55);
+    public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(3.5);
 
     // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
     // This may need to be tuned to your individual robot
