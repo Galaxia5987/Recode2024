4055d6fd06bb0d14db9b4a59951e45eeaa6c0859
noga <noga.veksler@gmail.com>
2024-12-11 19:44:48 +0200

    Add GAINS and FIRST_STAGE_RATIO

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
@@ -1,5 +1,11 @@
 package frc.robot.subsystems.elevator
 
+import frc.robot.lib.Gains
+import frc.robot.lib.selectGainsBasedOnMode
+
 const val MAX_HEIGHT = 1.3
 const val GEAR_RATIO = 0.2
 const val FIRSTSTAGE_GEARRATIOE = 0.5
+
+const val FIRST_STAGE_RATIO = 0.5
+val GAINS = selectGainsBasedOnMode(Gains(), Gains())
