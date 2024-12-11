29cc9878fcec58a4b0d0d8e1dceb55b34b03b160
noga <noga.veksler@gmail.com>
2024-12-11 19:46:06 +0200

    Change GEAR_RATIO

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
@@ -4,8 +4,6 @@ import frc.robot.lib.Gains
 import frc.robot.lib.selectGainsBasedOnMode
 
 const val MAX_HEIGHT = 1.3
-const val GEAR_RATIO = 0.2
-const val FIRSTSTAGE_GEARRATIOE = 0.5
-
+const val GEAR_RATIO = (1/12)*(42/48)
 const val FIRST_STAGE_RATIO = 0.5
 val GAINS = selectGainsBasedOnMode(Gains(), Gains())
