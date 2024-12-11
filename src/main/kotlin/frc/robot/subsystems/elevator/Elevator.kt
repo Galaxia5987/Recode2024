fa83caa08cf122a785b575b4de3550b65dc404fe
noga <noga.veksler@gmail.com>
2024-12-11 19:33:33 +0200

    add companion object

--- src/main/kotlin/frc/robot/subsystems/elevator/Elevator.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/Elevator.kt
@@ -5,7 +5,23 @@ import org.littletonrobotics.junction.AutoLogOutput
 
 class Elevator private constructor(private val io: ElevatorIO) : SubsystemBase() {
 
+    companion object {
+        @Volatile
+        private var instance: Elevator? = null
 
+        fun initialize(io:ElevatorIO){
+            synchronized(this) {
+            if (Elevator.instance == null) {
+                instance = Elevator(io)
+            }
+            }
+        }
+        fun getInstance(): Elevator {
+            return Elevator.instance ?: throw IllegalStateException(
+                "Elevator has not been initialized. Call initialize(io: ElevatorIO) first."
+            )
+        }
+    }
 
     fun setPosition(position: Double) {
         io.setPosition(position)
